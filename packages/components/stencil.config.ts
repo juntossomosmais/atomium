077ed097613c9eb1327522b47ad54f0e8552f3ab
felipefialho <hi@felipefialho.com>
2023-03-06 16:06:42 -0300

    feat: enable three shaking to each component

--- packages/components/stencil.config.ts
+++ packages/components/stencil.config.ts
@@ -1,32 +1,54 @@
 import { Config } from '@stencil/core'
+import { OutputTargetCustom } from '@stencil/core/internal'
 import { reactOutputTarget } from '@stencil/react-output-target'
 import { vueOutputTarget } from '@stencil/vue-output-target'
+import * as fs from 'fs'
+import * as path from 'path'
+
+const componentsDir = path.join(__dirname, 'src', 'components')
+
+const componentOutputTargets = fs
+  .readdirSync(componentsDir)
+  .filter((file) => fs.statSync(path.join(componentsDir, file)).isDirectory())
+  .map((componentDir) => ({
+    type: 'dist',
+    esm: {
+      dir: `dist/${componentDir}/esm`,
+    },
+    injectGlobalPaths: [
+      `${componentsDir}/${componentDir}/${componentDir}.scss`,
+    ],
+    empty: true,
+    minify: true,
+  }))
 
 export const config: Config = {
   namespace: 'atomium',
   globalScript: 'src/global/global.ts',
   globalStyle: 'src/global/global.scss',
   outputTargets: [
-    {
-      type: 'dist',
-      esmLoaderPath: '../loader',
-      copy: [{ src: 'assets' }],
-    },
+    ...componentOutputTargets,
     {
       type: 'dist-custom-elements',
+    },
+    {
+      type: 'dist',
+      esmLoaderPath: 'loader',
+      copy: [{ src: 'assets' }],
     },
     {
       type: 'docs-readme',
     },
     vueOutputTarget({
-      componentCorePackage: '@my-design-system/core',
-      proxiesFile: 'dist/vue/index.js',
-      includeDefineCustomElements: true,
+      componentCorePackage: '@atomium',
+      proxiesFile: 'dist/vue/index.ts',
     }),
     reactOutputTarget({
-      componentCorePackage: '@my-design-system/core',
-      proxiesFile: 'dist/react/index.js',
+      componentCorePackage: '@atomium',
+      proxiesFile: 'dist/react/index.ts',
       includeDefineCustomElements: true,
+      loaderDir: 'loader',
+      includePolyfills: true,
     }),
-  ],
+  ] as OutputTargetCustom[],
 }
