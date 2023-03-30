1628bfe50e6fca76e8c47b9b2ea08cf1b032faa5
felipefialho <hi@felipefialho.com>
2023-03-30 18:14:34 -0300

    feat(storybook): init improves of uiux of docs

--- packages/storybook/.storybook/preview.ts
+++ packages/storybook/.storybook/preview.ts
@@ -3,7 +3,7 @@ import { defineCustomElements } from 'atomium/loader'
 import '@ionic/core/css/core.css'
 import 'atomium-tokens/dist/styles.css'
 
-import './storybook.css'
+import './preview.css'
 
 defineCustomElements()
 
