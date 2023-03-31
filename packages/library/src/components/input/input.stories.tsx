c0ed1161ca8d99478e6c86a14af8d59bc4b1ea15
felipefialho <hi@felipefialho.com>
2023-03-31 17:28:18 -0300

    docs(input): use controls instead knobs

--- packages/library/src/components/input/input.stories.tsx
+++ packages/library/src/components/input/input.stories.tsx
@@ -1,4 +1,5 @@
-import { boolean, select, text, withKnobs } from '@storybook/addon-knobs'
+import { Meta } from '@storybook/html'
+import { AtoInput } from 'atomium/dist/types'
 
 import readme from './readme.md'
 
@@ -7,131 +8,138 @@ export default {
   parameters: {
     notes: readme,
   },
-  decorators: [withKnobs],
-}
-
-export const AtoInput = () => {
-  const colorOptions = {
-    Default: '',
-    Primary: 'primary',
-    Secondary: 'secondary',
-    Tertiary: 'tertiary',
-    Success: 'success',
-    Warning: 'warning',
-    Danger: 'danger',
-    Light: 'light',
-    Medium: 'medium',
-    Dark: 'dark',
-  }
-  const modeOptions = {
-    IOS: 'ios',
-    MD: 'md',
-  }
-  const expandOptions = {
-    None: '',
-    Block: 'block',
-  }
-  const fillOptions = {
-    Default: 'default',
-    Outline: 'outline',
-    Solid: 'solid',
-  }
-  const sizeOptions = {
-    Small: 'small',
-    Default: '',
-    Large: 'large',
-  }
-  const color = select('Color', colorOptions, 'Default')
-  const mode = select('Mode', modeOptions, 'md')
-  const expand = select('Expand', expandOptions, '')
-  const fill = select('Fill', fillOptions, 'Default')
-  const size = select('Size', sizeOptions, '')
-  const disabled = boolean('Disabled', false)
-  const placeholder = text('Placeholder', 'Enter text')
-  const type = select('Type', { Text: 'text', Password: 'password' }, 'text')
-  const autofocus = boolean('Autofocus', false)
-  const clearInput = boolean('Clear Input', false)
-  const clearOnEdit = boolean('Clear On Edit', false)
-  const pattern = text('Pattern', '')
-  const readonly = boolean('Readonly', false)
-  const required = boolean('Required', false)
-  const spellcheck = boolean('Spellcheck', false)
-  const autocapitalize = select(
-    'Autocapitalize',
-    {
-      None: 'none',
-      Sentences: 'sentences',
-      Words: 'words',
-      Characters: 'characters',
+  argTypes: {
+    color: {
+      control: {
+        type: 'select',
+        options: ['primary', 'secondary'],
+      },
     },
-    'none'
-  )
-  const autocomplete = select('Autocomplete', { On: 'on', Off: 'off' }, 'on')
-  const inputmode = select(
-    'Inputmode',
-    {
-      None: 'none',
-      Decimal: 'decimal',
-      Numeric: 'numeric',
-      Tel: 'tel',
-      Search: 'search',
-      Text: 'text',
-      Url: 'url',
-      Email: 'email',
+    mode: {
+      control: {
+        type: 'select',
+        options: ['ios', 'md'],
+      },
     },
-    'none'
-  )
-  const iconStart = text('Icon Start', '')
-  const iconEnd = text('Icon End', '')
+    expandable: {
+      control: {
+        type: 'boolean',
+      },
+    },
+    fill: {
+      control: {
+        type: 'select',
+        options: ['default', 'outline', 'solid'],
+      },
+    },
+    size: {
+      control: {
+        type: 'select',
+        options: ['small', 'medium', 'large'],
+      },
+    },
+    disabled: {
+      control: 'boolean',
+    },
+    placeholder: {
+      control: 'text',
+    },
+    type: {
+      control: {
+        type: 'inline-radio',
+        options: { text: 'text', password: 'password' },
+      },
+    },
+    autofocus: {
+      control: 'boolean',
+    },
+    clearInput: {
+      control: 'boolean',
+    },
+    clearOnEdit: {
+      control: 'boolean',
+    },
+    pattern: {
+      control: 'text',
+    },
+    readonly: {
+      control: 'boolean',
+    },
+    required: {
+      control: 'boolean',
+    },
+    spellcheck: {
+      control: 'boolean',
+    },
+    autocapitalize: {
+      control: {
+        type: 'select',
+        options: ['none', 'sentences', 'words', 'characters'],
+      },
+    },
+    autocomplete: {
+      control: {
+        type: 'boolean',
+      },
+    },
+    inputmode: {
+      control: {
+        type: 'select',
+        options: [
+          'none',
+          'decimal',
+          'numeric',
+          'tel',
+          'search',
+          'text',
+          'url',
+          'email',
+        ],
+      },
+    },
+    labelPosition: {
+      control: {
+        type: 'select',
+        options: ['fixed', 'stacked', 'floating'],
+      },
+    },
+  },
+} as Meta
 
-  const positionOptions = {
-    Fixed: 'fixed',
-    Stacked: 'stacked',
-    Floating: 'floating',
-  }
-
-  const labelPosition = select('Label Position', positionOptions, 'floating')
-  const labelText = text('Label Text', 'Label Text')
-
+export const Default = (args: AtoInput) => {
   return `
     <ato-input
-      label-text="${labelText}"
-      label-position="${labelPosition}"
-      fill="${fill}"
-      color="${color}"
-      mode="${mode}"
-      expand="${expand}"
-      size="${size}"
-      disabled="${disabled}"
-      placeholder="${placeholder}"
-      type="${type}"
-      autofocus="${autofocus}"
-      clear-input="${clearInput}"
-      clear-on-edit="${clearOnEdit}"
-      pattern="${pattern}"
-      readonly="${readonly}"
-      required="${required}"
-      spellcheck="${spellcheck}"
-      autocapitalize="${autocapitalize}"
-      autocomplete="${autocomplete}"
-      inputmode="${inputmode}"
-      icon-start="${iconStart}"
-      icon-end="${iconEnd}"
+      label-text="${args.labelText}"
+      label-position="${args.labelPosition}"
+      fill="${args.fill}"
+      color="${args.color}"
+      mode="${args.mode}"
+      expandable="${args.expandable}"
+      size="${args.size}"
+      disabled="${args.disabled}"
+      placeholder="${args.placeholder}"
+      type="${args.type}"
+      autofocus="${args.autofocus}"
+      clear-input="${args.clearInput}"
+      clear-on-edit="${args.clearOnEdit}"
+      pattern="${args.pattern}"
+      readonly="${args.readonly}"
+      required="${args.required}"
+      spellcheck="${args.spellcheck}"
+      autocapitalize="${args.autocapitalize}"
+      autocomplete="${args.autocomplete}"
+      inputmode="${args.inputmode}"
     />
-
-    <script>
-      document.querySelector('ato-input').addEventListener('atoChange', (e) => {
-        console.log('<ato-input> atoChange:', e.detail.value)
-      })
-
-      document.querySelector('ato-input').addEventListener('atoFocus', () => {
-        console.log("<ato-input> atoFocus using focus!")
-      })
-
-      document.querySelector('ato-input').addEventListener('atoBlur', () => {
-        console.log("<ato-input> atoBlur using blur!")
-      })
-    </script>
-
   `
+}
+
+Default.args = {
+  labelText: 'Example input',
+  labelPosition: 'floating',
+  fill: 'default',
+  color: 'primary',
+  mode: 'md',
+  disabled: false,
+  placeholder: 'Placeholder',
+  readonly: false,
 }
