473fbe64f95f771f657629060e015b08a11b90ae
felipefialho <hi@felipefialho.com>
2023-03-31 15:47:54 -0300

    docs(button): use controls instead knobs

--- packages/library/src/components/button/button.stories.tsx
+++ packages/library/src/components/button/button.stories.tsx
@@ -1,4 +1,5 @@
-import { boolean, select, text, withKnobs } from '@storybook/addon-knobs'
+import { Meta } from '@storybook/html'
+import { AtoButton } from 'atomium/dist/types'
 
 import readme from './readme.md'
 
@@ -7,43 +8,75 @@ export default {
   parameters: {
     notes: readme,
   },
-  decorators: [withKnobs],
+  argTypes: {
+    color: {
+      control: {
+        type: 'select',
+        options: ['primary', 'secondary', 'tertiary'],
+      },
+    },
+    fill: {
+      control: {
+        type: 'select',
+        options: ['clear', 'outline', 'solid'],
+      },
+    },
+    shape: {
+      control: {
+        type: 'select',
+        options: ['clear', 'outline', 'solid'],
+      },
+    },
+    size: {
+      control: {
+        type: 'select',
+        options: ['small', 'default', 'large'],
+      },
+    },
+    disabled: {
+      control: {
+        type: 'boolean',
+      },
+    },
+    type: {
+      control: {
+        type: 'select',
+        options: ['submit', 'reset', 'button'],
+      },
+    },
+    mode: {
+      control: {
+        type: 'select',
+        options: ['md', 'ios', 'md'],
+      },
+    },
+  },
+} as Meta
+
+type argsButton = AtoButton & {
+  label: string
 }
 
-export const AtoButton = () => {
-  const colorOptions = {
-    Default: '',
-    Red: 'red',
-    Green: 'green',
-    Blue: 'blue',
-  }
-  const fillOptions = {
-    Solid: 'solid',
-    Outline: 'outline',
-    Clear: 'clear',
-  }
-  const sizeOptions = {
-    Default: 'default',
-    Small: 'small',
-    Large: 'large',
-  }
-  const color = select('Color', colorOptions, '')
-  const fill = select('Fill', fillOptions, 'solid')
-  const size = select('Size', sizeOptions, 'default')
-  const disabled = boolean('Disabled', false)
-  const type = select(
-    'Type',
-    { Button: 'button', Submit: 'submit', Reset: 'reset' },
-    'button'
-  )
+export const Default = (args: argsButton) => `
+  <ato-button
+    color="${args.color}"
+    fill="${args.fill}"
+    shape="${args.shape}"
+    size="${args.size}"
+    disabled="${args.disabled}"
+    type="${args.type}"
+    mode="${args.mode}"
+  >
+    ${args.label}
+  </ato-button>
+`
 
-  return `
-    <ato-button
-      color=${color}
-      fill=${fill}
-      size=${size}
-      type=${type}
-      disabled=${disabled}>
-        ${text('Label', 'Click me')}
-    </ato-button>`
+Default.args = {
+  label: 'Click me!',
+  color: 'primary',
+  fill: 'solid',
+  size: 'default',
+  disabled: false,
+  type: 'button',
+  mode: 'md',
 }
