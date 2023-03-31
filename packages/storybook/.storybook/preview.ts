623c7ec98b280248cf01feb3b78c3dec4e6eef87
felipefialho <hi@felipefialho.com>
2023-03-31 17:47:35 -0300

    chore: remove unuse default actions and controls

--- packages/storybook/.storybook/preview.ts
+++ packages/storybook/.storybook/preview.ts
@@ -6,13 +6,3 @@ import 'atomium-tokens/dist/styles.css'
 import './preview.css'
 
 defineCustomElements()
-
-export const parameters = {
-  actions: { argTypesRegex: '^on|ato|ion.*' },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
-}
