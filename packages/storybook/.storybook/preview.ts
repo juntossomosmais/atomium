fd57b6a7f605eb69e20bcfac7db7590fdf71daec
felipefialho <hi@felipefialho.com>
2023-04-03 14:51:10 -0300

    feat(storybook): add custom viewports to addon-viewport

--- packages/storybook/.storybook/preview.ts
+++ packages/storybook/.storybook/preview.ts
@@ -1,3 +1,5 @@
+import { addParameters } from '@storybook/html'
+
 import { defineCustomElements } from 'atomium/loader'
 
 import '@ionic/core/css/core.css'
@@ -6,3 +8,40 @@ import 'atomium-tokens/dist/styles.css'
 import './preview.css'
 
 defineCustomElements()
+
+const customViewports = {
+  small: {
+    name: 'Small',
+    styles: {
+      height: '600px',
+      width: '576px',
+    },
+  },
+  medium: {
+    name: 'Medium',
+    styles: {
+      height: '600px',
+      width: '768px',
+    },
+  },
+  large: {
+    name: 'Large',
+    styles: {
+      height: '600px',
+      width: '992px',
+    },
+  },
+  xlarge: {
+    name: 'Extra Large',
+    styles: {
+      height: '600px',
+      width: '1200px',
+    },
+  },
+}
+
+addParameters({
+  viewport: {
+    viewports: customViewports,
+  },
+})
