f92c3285792e1eb88cdccd4b2591b2a72a79347b
felipefialho <hi@felipefialho.com>
2023-04-05 19:35:00 -0300

    feat(docs): load css from atomium

--- apps/docs/.storybook/preview.ts
+++ apps/docs/.storybook/preview.ts
@@ -1,7 +1,7 @@
 import { defineCustomElements } from 'atomium/loader'
 
-import '@ionic/core/css/core.css'
 import 'atomium-tokens/dist/styles.css'
+import 'atomium/dist/atomium/atomium.css'
 
 import './preview.css'
 
