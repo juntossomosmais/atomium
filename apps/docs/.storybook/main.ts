054d0d791ac6477f2c849595062c2c527da78239
felipefialho <hi@felipefialho.com>
2023-04-05 14:28:52 -0300

    chore(docs): remove unuse deps

--- apps/docs/.storybook/main.ts
+++ apps/docs/.storybook/main.ts
@@ -5,10 +5,9 @@ module.exports = {
     '../../../packages/tokens/stories/**/*.stories.@(js|jsx|ts|tsx|mdx)',
   ],
   addons: [
-    '@storybook/addon-docs',
     '@storybook/addon-essentials',
+    '@storybook/addon-docs',
     '@storybook/addon-actions',
-    '@storybook/addon-links',
     '@storybook/addon-a11y',
     '@storybook/addon-viewport',
     '@storybook/theming',
