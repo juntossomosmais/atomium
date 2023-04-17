4860e258faf4c96b71bd5a55edfc62d4e173d7b6
felipefialho <hi@felipefialho.com>
2023-04-17 18:57:41 -0300

    chore: update paths with @scope

--- apps/docs/.storybook/preview.ts
+++ apps/docs/.storybook/preview.ts
@@ -2,8 +2,8 @@ import { defineCustomElements } from '../../../packages/core/loader'
 
 import DocumentationTemplate from './DocumentationTemplate.mdx'
 
-import '@atomium/core/dist/atomium/atomium.css'
-import '@atomium/tokens/dist/styles.css'
+import '@juntossomosmais/atomium-tokens/dist/styles.css'
+import '@juntossomosmais/atomium/dist/atomium/atomium.css'
 
 defineCustomElements()
 
