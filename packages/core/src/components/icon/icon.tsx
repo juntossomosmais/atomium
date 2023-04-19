cdfdb54ff09ae3af5104ee6e4f01acec3a306c6a
Mauricio Mutte <mauriciomutte.mm@gmail.com>
2023-04-19 11:36:47 -0300

    feat: create Icon component using Stencil

---
+++ packages/core/src/components/icon/icon.tsx
@@ -0,0 +1,24 @@
+import { Color } from '@ionic/core'
+import { Component, Prop, h } from '@stencil/core'
+
+const CDN_URL = 'https://unpkg.com/ionicons@7.1.0/dist/ionicons/svg/'
+
+@Component({
+  tag: 'ato-icon',
+  shadow: true,
+})
+export class AtoIcon {
+  @Prop() icon?: string
+  @Prop() color?: Color
+  @Prop() size?: 'small' | 'large'
+
+  render() {
+    return (
+      <ion-icon
+        icon={`${CDN_URL}/${this.icon}.svg`}
+        color={this.color}
+        size={this.size}
+      />
+    )
+  }
+}
