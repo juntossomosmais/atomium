df9c52ad4ecdfb63997d99d1897a63851c6ab7e8
felipefialho <hi@felipefialho.com>
2023-04-26 17:13:23 -0300

    chore(utils): create library to share storybook configs

---
+++ utils/storybook/custom-viewports.ts
@@ -0,0 +1,30 @@
+export const CustomViewports = {
+  small: {
+    name: 'Small',
+    styles: {
+      height: '600px',
+      width: '576px',
+    },
+  },
+  medium: {
+    name: 'Medium',
+    styles: {
+      height: '600px',
+      width: '768px',
+    },
+  },
+  large: {
+    name: 'Large',
+    styles: {
+      height: '600px',
+      width: '992px',
+    },
+  },
+  xlarge: {
+    name: 'Extra Large',
+    styles: {
+      height: '600px',
+      width: '1200px',
+    },
+  },
+}
