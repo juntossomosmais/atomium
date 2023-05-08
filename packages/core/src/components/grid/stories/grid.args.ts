66cc092e8a0002a9147c6460f64d46fe20809507
felipefialho <hi@felipefialho.com>
2023-05-08 19:17:02 -0300

    docs(core): add category inside props

--- packages/core/src/components/grid/stories/grid.args.ts
+++ packages/core/src/components/grid/stories/grid.args.ts
@@ -1,5 +1,7 @@
 import { withActions } from '@storybook/addon-actions/decorator'
 
+import { Category } from '@atomium/storybook-utils/enums/table'
+
 export const GridStoryArgs = {
   decorators: [withActions],
   parameters: {
@@ -15,22 +17,37 @@ export const GridStoryArgs = {
       description:
         'If `true`, the Grid will be fixed and will have a consistent width based on the screen size.',
       defaultValue: { summary: false },
+      table: {
+        category: Category.PROPERTIES,
+      },
     },
     size: {
       description:
         'Columns can be set to specific sizes to take up a certain number out of the total number of columns, or resize their width based on the content, it accepts responsive values like `size-sm`, `size-md`, `size-lg`, `size-xl`',
+      table: {
+        category: Category.PROPERTIES,
+      },
     },
     offset: {
       description:
         'Columns can be offset to the right by a certain number of columns, it accepts responsive values like `offset-sm`, `offset-md`, `offset-lg`, `offset-xl`',
+      table: {
+        category: Category.PROPERTIES,
+      },
     },
     pull: {
       description:
         'Columns can be pulled to the left by a certain number of columns',
+      table: {
+        category: Category.PROPERTIES,
+      },
     },
     push: {
       description:
         'Columns can be pushed to the right by a certain number of columns',
+      table: {
+        category: Category.PROPERTIES,
+      },
     },
   },
 }
