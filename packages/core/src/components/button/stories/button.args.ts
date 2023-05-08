5fcde32de7da430cd06ca6be2205642ea90bb019
felipefialho <hi@felipefialho.com>
2023-05-08 19:16:53 -0300

    docs(core): add category inside props

--- packages/core/src/components/button/stories/button.args.ts
+++ packages/core/src/components/button/stories/button.args.ts
@@ -1,5 +1,7 @@
 import { withActions } from '@storybook/addon-actions/decorator'
 
+import { Category } from '@atomium/storybook-utils/enums/table'
+
 export const ButtonStoryArgs = {
   parameters: {
     actions: {
@@ -11,12 +13,18 @@ export const ButtonStoryArgs = {
     label: {
       control: 'text',
       description: 'The label of the button',
+      table: {
+        category: Category.PROPERTIES,
+      },
     },
     color: {
       control: 'select',
       options: ['primary', 'secondary'],
       defaultValue: { summary: 'primary' },
       description: "The color to use from your application's color palette.",
+      table: {
+        category: Category.PROPERTIES,
+      },
     },
     fill: {
       control: 'select',
@@ -24,56 +32,92 @@ export const ButtonStoryArgs = {
       defaultValue: { summary: 'solid' },
       description:
         'Set to `"clear"` for a transparent button that resembles a flat button, to `"outline"` for a transparent button with a border, or to `"solid"` for a button with a filled background. The default fill is "solid" except inside of a toolbar, where the default is `"clear"`.',
+      table: {
+        category: Category.PROPERTIES,
+      },
     },
     expand: {
       control: 'select',
       options: ['none', 'block'],
       description: 'Set to `"block"` for a full-width button.',
+      table: {
+        category: Category.PROPERTIES,
+      },
     },
     size: {
       control: 'select',
       options: ['small', 'default', 'large'],
       defaultValue: { summary: 'default' },
       description: 'The size of the button.',
+      table: {
+        category: Category.PROPERTIES,
+      },
     },
     disabled: {
       control: 'boolean',
       description: 'If `true`, the user cannot interact with the button.',
+      table: {
+        category: Category.PROPERTIES,
+      },
     },
     loading: {
       control: 'boolean',
       description: 'If `true`, the button will be show a loading indicator.',
+      table: {
+        category: Category.PROPERTIES,
+      },
     },
     href: {
       description:
         'Contains a URL or a URL fragment that the hyperlink points to. If this property is set, an anchor tag will be rendered.',
+      table: {
+        category: Category.PROPERTIES,
+      },
     },
     rel: {
       description:
         'The relationship of the linked URL as space-separated link types.',
+      table: {
+        category: Category.PROPERTIES,
+      },
     },
     target: {
       description:
         'Specifies where to display the linked URL. Only applies when an href is provided. Special keywords: `"_blank"`, `"_self"`, `"_parent"`, `"_top"`.',
+      table: {
+        category: Category.PROPERTIES,
+      },
     },
     download: {
       description: 'This attribute instructs browsers to download a URL.',
+      table: {
+        category: Category.PROPERTIES,
+      },
     },
     type: {
       control: 'select',
       options: ['none', 'submit', 'reset', 'button'],
       defaultValue: { summary: 'button' },
       description: 'The type of the button.',
+      table: {
+        category: Category.PROPERTIES,
+      },
     },
     mode: {
       control: 'select',
       options: ['md', 'ios'],
       defaultValue: { summary: 'md' },
       description: 'The mode determines which platform styles to use.',
+      table: {
+        category: Category.PROPERTIES,
+      },
     },
     atomClick: {
       action: 'atomClick',
       description: 'Emitted when the button is clicked.',
+      table: {
+        category: 'Events',
+      },
     },
   },
 }
