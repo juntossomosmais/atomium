a8fb1655ced7a809a8e9573dd9bfc73716881be0
felipefialho <hi@felipefialho.com>
2023-05-09 17:09:59 -0300

    docs(core): use storybook/web-components instead storybook/html

--- packages/core/src/components/button/stories/button.core.stories.tsx
+++ packages/core/src/components/button/stories/button.core.stories.tsx
@@ -1,4 +1,6 @@
-import { Meta, StoryObj } from '@storybook/html'
+import { Meta, StoryObj } from '@storybook/web-components'
+
+import { html } from 'lit'
 
 import { ButtonComponentArgs, ButtonStoryArgs } from './button.args'
 
@@ -8,20 +10,20 @@ export default {
 } as Meta
 
 const createButton = (args) => {
-  return `
-<atom-button
-  color="${args.color}"
-  fill="${args.fill}"
-  expand="${args.expand}"
-  size="${args.size}"
-  disabled="${args.disabled}"
-  loading="${args.loading}"
-  type="${args.type}"
-  mode="${args.mode}"
->
-  ${args.label}
-</atom-button>
-`
+  return html`
+    <atom-button
+      color="${args.color}"
+      fill="${args.fill}"
+      expand="${args.expand}"
+      size="${args.size}"
+      disabled="${args.disabled}"
+      loading="${args.loading}"
+      type="${args.type}"
+      mode="${args.mode}"
+    >
+      ${args.label}
+    </atom-button>
+  `
 }
 
 export const Primary: StoryObj = {
