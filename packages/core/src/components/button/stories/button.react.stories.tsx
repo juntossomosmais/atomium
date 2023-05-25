a8e8a5f55c1fa9fb5cec1c34f4f4852873a1ecd6
Maurício Mutte <mauriciomutte.mm@gmail.com>
2023-05-25 11:18:11 -0300

    feat(core): add Button shape circle (#133)

--- packages/core/src/components/button/stories/button.react.stories.tsx
+++ packages/core/src/components/button/stories/button.react.stories.tsx
@@ -1,7 +1,7 @@
 import { Meta, StoryObj } from '@storybook/react'
 import React from 'react'
 
-import { AtomButton } from '@juntossomosmais/atomium/react'
+import { AtomButton, AtomIcon } from '@juntossomosmais/atomium/react'
 
 import { ButtonComponentArgs, ButtonStoryArgs } from './button.args'
 
@@ -48,5 +48,29 @@ export const Text: StoryObj = {
     ...Primary.args,
     color: 'secondary',
     fill: 'clear',
+  },
+}
+
+export const CircleButton: StoryObj = {
+  render: (args: any) => (
+    <AtomButton
+      color={args.color}
+      fill={args.fill}
+      size={args.size}
+      disabled={args.disabled}
+      loading={args.loading}
+      type={args.type}
+      mode={args.mode}
+      expand={args.expand}
+      shape={args.shape}
+    >
+      <AtomIcon icon="heart" />
+    </AtomButton>
+  ),
+  args: {
+    ...Primary.args,
+    color: 'secondary',
+    fill: 'outline',
+    shape: 'circle',
   },
 }
