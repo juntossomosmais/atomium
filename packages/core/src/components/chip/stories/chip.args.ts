9ece5fe726c68634d66986d56d573d085188d757
felipefialho <hi@felipefialho.com>
2023-05-29 16:29:35 -0300

    docs(chip): add docs to component

---
+++ packages/core/src/components/chip/stories/chip.args.ts
@@ -0,0 +1,65 @@
+import { withActions } from '@storybook/addon-actions/decorator'
+
+import { Category } from '@atomium/storybook-utils/enums/table'
+
+export const ChipStoryArgs = {
+  decorators: [withActions],
+  parameters: {
+    docs: {
+      description: {
+        component:
+          'Wrapper of Ionic Chip component. Read the [Ionic documentation](https://ionicframework.com/docs/api/chip) for more information about the available properties and possibilities.',
+      },
+    },
+  },
+  argTypes: {
+    disabled: {
+      control: 'boolean',
+      defaultValue: { summary: false },
+      description: 'If true, the user cannot interact with the chip.',
+      table: {
+        category: Category.PROPERTIES,
+      },
+    },
+    activated: {
+      control: 'boolean',
+      defaultValue: { summary: false },
+      description: 'If true, the chip will be activated.',
+      table: {
+        category: Category.PROPERTIES,
+      },
+    },
+    icon: {
+      control: 'text',
+      description: 'If have an value, the chip will have an icon.',
+      table: {
+        category: Category.PROPERTIES,
+      },
+    },
+    close: {
+      control: 'boolean',
+      defaultValue: { summary: false },
+      description: 'If true, the chip will have a close button.',
+      table: {
+        category: Category.PROPERTIES,
+      },
+    },
+    mode: {
+      control: 'select',
+      options: ['md', 'ios'],
+      defaultValue: { summary: 'md' },
+      description: 'The mode determines which platform styles to use.',
+      table: {
+        category: Category.PROPERTIES,
+      },
+    },
+  },
+}
+
+export const ChipComponentArgs = {
+  mode: 'md',
+  disabled: false,
+  activated: false,
+  close: false,
+  label: 'Label',
+}
