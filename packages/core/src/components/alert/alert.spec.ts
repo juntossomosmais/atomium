0ccf2361c31713231f62aee4ca6407f1bda1f26f
felipefialho <hi@felipefialho.com>
2023-06-05 17:13:55 -0300

    test(alert): add component

---
+++ packages/core/src/components/alert/alert.spec.ts
@@ -0,0 +1,229 @@
+import { newSpecPage } from '@stencil/core/testing'
+import { AtomAlert } from './alert'
+
+const messageTitle = 'Alert title'
+const messageText = 'Alert text'
+
+const createAlertColor = (color: string) => {
+  return `
+    <mock:shadow-root>
+      <div class="atom-alert atom-color--${color}">
+        <div class="atom-body">
+          <div class="atom-content">
+            <p class="atom-title">
+              Alert title
+            </p>
+            <p class="atom-message">
+              Alert text
+            </p>
+          </div>
+        </div>
+        <slot></slot>
+        <div class="atom-actions">
+          <slot name="actions"></slot>
+        </div>
+      </div>
+    </mock:shadow-root>
+  `
+}
+
+describe('AtomAlert', () => {
+  it('should renders with default props', async () => {
+    const page = await newSpecPage({
+      components: [AtomAlert],
+      html: `<atom-alert message-title="${messageTitle}" message-text="${messageText}"></atom-alert>`,
+    })
+
+    await page.waitForChanges()
+
+    expect(page.root).toEqualHtml(`
+      <atom-alert message-title="${messageTitle}" message-text="${messageText}"  role="alert">
+        ${createAlertColor('neutral')}
+      </atom-alert>
+    `)
+  })
+
+  it('should add "atom-color--info" class when color prop is info', async () => {
+    const page = await newSpecPage({
+      components: [AtomAlert],
+      html: `<atom-alert color="info" message-title="${messageTitle}" message-text="${messageText}"></atom-alert>`,
+    })
+
+    await page.waitForChanges()
+
+    expect(page.root).toEqualHtml(`
+      <atom-alert color="info" message-text="Alert text" message-title="Alert title" role="alert">
+        ${createAlertColor('info')}
+      </atom-alert>
+    `)
+  })
+
+  it('should add "atom-color--success" class when color prop is success', async () => {
+    const page = await newSpecPage({
+      components: [AtomAlert],
+      html: `<atom-alert color="success" message-title="${messageTitle}" message-text="${messageText}"></atom-alert>`,
+    })
+
+    await page.waitForChanges()
+
+    expect(page.root).toEqualHtml(`
+      <atom-alert color="success" message-text="Alert text" message-title="Alert title" role="alert">
+        ${createAlertColor('success')}
+      </atom-alert>
+    `)
+  })
+
+  it('should add "atom-color--danger" class when color prop is danger', async () => {
+    const page = await newSpecPage({
+      components: [AtomAlert],
+      html: `<atom-alert color="danger" message-title="${messageTitle}" message-text="${messageText}"></atom-alert>`,
+    })
+
+    await page.waitForChanges()
+
+    expect(page.root).toEqualHtml(`
+      <atom-alert color="danger" message-text="Alert text" message-title="Alert title" role="alert">
+        ${createAlertColor('danger')}
+      </atom-alert>
+    `)
+  })
+
+  it('should add "atom-color--warning" class when color prop is warning', async () => {
+    const page = await newSpecPage({
+      components: [AtomAlert],
+      html: `<atom-alert color="warning" message-title="${messageTitle}" message-text="${messageText}"></atom-alert>`,
+    })
+
+    await page.waitForChanges()
+
+    expect(page.root).toEqualHtml(`
+      <atom-alert color="warning" message-text="Alert text" message-title="Alert title" role="alert">
+        ${createAlertColor('warning')}
+      </atom-alert>
+    `)
+  })
+
+  it('should add icon when icon prop is set', async () => {
+    const page = await newSpecPage({
+      components: [AtomAlert],
+      html: `<atom-alert message-title="${messageTitle}" message-text="${messageText}" icon="alert"></atom-alert>`,
+    })
+
+    await page.waitForChanges()
+
+    expect(page.root).toEqualHtml(`
+      <atom-alert icon="alert" message-title="${messageTitle}" message-text="${messageText}" role="alert">
+        <mock:shadow-root>
+            <div class="atom-alert atom-color--neutral">
+              <div class="atom-body">
+                <atom-icon class="atom-icon" icon="alert"></atom-icon>
+                <div class="atom-content">
+                <p class="atom-title">
+                  Alert title
+                </p>
+                <p class="atom-message">
+                  Alert text
+                </p>
+              </div>
+            </div>
+            <slot></slot>
+            <div class="atom-actions">
+              <slot name="actions"></slot>
+            </div>
+          </div>
+        </mock:shadow-root>
+      </atom-alert>
+    `)
+  })
+
+  it('should add close button when close prop is true', async () => {
+    const page = await newSpecPage({
+      components: [AtomAlert],
+      html: `<atom-alert message-title="${messageTitle}" message-text="${messageText}" close="true"></atom-alert>`,
+    })
+
+    await page.waitForChanges()
+
+    expect(page.root).toEqualHtml(`
+      <atom-alert close="true" message-title="${messageTitle}" message-text="${messageText}" role="alert">
+          <mock:shadow-root>
+            <div class="atom-alert atom-color--neutral">
+              <div class="atom-body">
+                <div class="atom-content">
+                <p class="atom-title">
+                  Alert title
+                </p>
+                <p class="atom-message">
+                  Alert text
+                </p>
+              </div>
+            </div>
+            <slot></slot>
+            <div class="atom-actions">
+              <slot name="actions"></slot>
+            </div>
+            <button aria-label="Close" class="atom-close">
+               <atom-icon icon="close"></atom-icon>
+            </button>
+          </div>
+        </mock:shadow-root>
+      </atom-alert>
+    `)
+  })
+
+  it('should add actions slot when actions prop is set', async () => {
+    const page = await newSpecPage({
+      components: [AtomAlert],
+      html: `<atom-alert message-title="${messageTitle}" message-text="${messageText}"><button slot="actions">Action</button></atom-alert>`,
+    })
+
+    await page.waitForChanges()
+
+    expect(page.root).toEqualHtml(`
+      <atom-alert message-title="${messageTitle}" message-text="${messageText}" role="alert">
+        <mock:shadow-root>
+          <div class="atom-alert atom-color--neutral">
+            <div class="atom-body">
+              <div class="atom-content">
+              <p class="atom-title">
+                Alert title
+              </p>
+              <p class="atom-message">
+                Alert text
+              </p>
+            </div>
+          </div>
+          <slot></slot>
+          <div class="atom-actions">
+            <slot name="actions"></slot>
+          </div>
+        </div>
+      </mock:shadow-root>
+      <button slot="actions">
+        Action
+      </button>
+    </atom-alert>
+    `)
+  })
+
+  it('emits atomClose event on alert close click', async () => {
+    const page = await newSpecPage({
+      components: [AtomAlert],
+      html: '<atom-alert close="true"></atom-alert>',
+    })
+
+    await page.waitForChanges()
+
+    const alertCloseEl = page.root?.shadowRoot?.querySelector(
+      '.atom-close'
+    ) as HTMLElement
+    const spy = jest.fn()
+
+    page.root?.addEventListener('atomClose', spy)
+    alertCloseEl?.click()
+
+    page.root?.dispatchEvent(new CustomEvent('atomClose'))
+
+    expect(spy).toHaveBeenCalled()
+  })
+})
