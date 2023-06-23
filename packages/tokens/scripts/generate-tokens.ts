75f85b480bb9e0cdc0cac093caa1a15eea9e0987
RodrigoRVSN <rodrigovictor81@gmail.com>
2023-06-23 15:48:30 -0300

    chore(tokens): add typescript to generate tokens script

--- packages/tokens/scripts/generate-tokens.ts
+++ packages/tokens/scripts/generate-tokens.ts
@@ -3,31 +3,37 @@ import path from 'path'
 import { fileURLToPath } from 'url'
 
 const CURRENT_DIR = path.dirname(fileURLToPath(import.meta.url))
-const TOKENS_DIR = path.resolve(CURRENT_DIR, '../src/tokens.scss')
-const OUTPUT_DIR = path.resolve(CURRENT_DIR, '../dist/')
+const TOKENS_DIR = path.resolve(CURRENT_DIR, '../tokens.css')
+const OUTPUT_DIR = path.resolve(CURRENT_DIR, '../')
 
 const prefixes = ['color', 'spacing', 'screen']
-const exportStatements = []
+const exportStatements: Array<string> = []
 
-function extractTokensFromCss(cssContent, prefix) {
+function extractTokensFromCss(cssContent: string, prefix: string) {
   const cssVariablePattern = new RegExp(`--(${prefix}[\\w-]+):\\s*([^;]+)`, 'g')
   const tokens = {}
 
-  cssContent.replace(cssVariablePattern, (_, variable, value) => {
-    const variableParts = variable.split('-')
-    const variableCamelCase = variableParts
-      .map((part, index) =>
-        index !== 0 ? part.charAt(0).toUpperCase() + part.slice(1) : part
-      )
-      .join('')
+  cssContent.replace(
+    cssVariablePattern,
+    (_, variable: string, value: string) => {
+      const variableParts = variable.split('-')
+      const variableCamelCase = variableParts
+        .map((part, index) =>
+          index !== 0 ? part.charAt(0).toUpperCase() + part.slice(1) : part
+        )
+        .join('')
 
-    tokens[variableCamelCase] = value.trim()
-  })
+      return (tokens[variableCamelCase] = value.trim())
+    }
+  )
 
   return tokens
 }
 
-function generateJavaScriptFile(tokens, outputFilePath) {
+function generateJavaScriptFile(
+  tokens: { [token: string]: string },
+  outputFilePath: string
+) {
   const jsCode = Object.entries(tokens)
     .map(([variable, value]) => `export const ${variable} = '${value}';`)
     .join('\n')
@@ -35,7 +41,7 @@ function generateJavaScriptFile(tokens, outputFilePath) {
   fs.writeFileSync(outputFilePath, jsCode, 'utf8')
 }
 
-function processCssFileByTokenPrefix(cssFilePath, prefix) {
+function processCssFileByTokenPrefix(cssFilePath: string, prefix: string) {
   const cssContent = fs.readFileSync(cssFilePath, 'utf8')
   const tokens = extractTokensFromCss(cssContent, prefix)
 
@@ -47,7 +53,7 @@ function processCssFileByTokenPrefix(cssFilePath, prefix) {
 }
 
 function generateIndexFile() {
-  const outputFileName = 'index.js'
+  const outputFileName = 'index.d.ts'
   const outputFilePath = path.join(OUTPUT_DIR, outputFileName)
   fs.writeFileSync(outputFilePath, exportStatements.join('\n'), 'utf8')
 }
