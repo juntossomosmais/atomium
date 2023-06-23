beb3e4501412ce96df65edb621cd7e07e759d9d3
RodrigoRVSN <rodrigovictor81@gmail.com>
2023-06-23 13:26:35 -0300

    feat(tokens): script to generate tokens variables

---
+++ packages/tokens/scripts/generateTokens.js
@@ -0,0 +1,59 @@
+import fs from 'fs'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const CURRENT_DIR = path.dirname(fileURLToPath(import.meta.url))
+const TOKENS_DIR = path.resolve(CURRENT_DIR, '../src/tokens.scss')
+const OUTPUT_DIR = path.resolve(CURRENT_DIR, '../dist/')
+
+const prefixes = ['color', 'spacing', 'screen']
+const exportStatements = []
+
+function extractTokensFromCss(cssContent, prefix) {
+  const cssVariablePattern = new RegExp(`--(${prefix}[\\w-]+):\\s*([^;]+)`, 'g')
+  const tokens = {}
+
+  cssContent.replace(cssVariablePattern, (_, variable, value) => {
+    const variableParts = variable.split('-')
+    const variableCamelCase = variableParts
+      .map((part, index) =>
+        index !== 0 ? part.charAt(0).toUpperCase() + part.slice(1) : part
+      )
+      .join('')
+
+    tokens[variableCamelCase] = value.trim()
+  })
+
+  return tokens
+}
+
+function generateJavaScriptFile(tokens, outputFilePath) {
+  const jsCode = Object.entries(tokens)
+    .map(([variable, value]) => `export const ${variable} = '${value}';`)
+    .join('\n')
+
+  fs.writeFileSync(outputFilePath, jsCode, 'utf8')
+}
+
+function processCssFileByTokenPrefix(cssFilePath = TOKENS_DIR, prefix) {
+  const cssContent = fs.readFileSync(cssFilePath, 'utf8')
+  const tokens = extractTokensFromCss(cssContent, prefix)
+
+  const outputFileName = `${prefix}.js`
+  const outputFilePath = path.join(OUTPUT_DIR, outputFileName)
+
+  generateJavaScriptFile(tokens, outputFilePath)
+  exportStatements.push(`export * from './${prefix}';`)
+}
+
+function generateIndexFile() {
+  const outputFileName = 'index.js'
+  const outputFilePath = path.join(OUTPUT_DIR, outputFileName)
+  fs.writeFileSync(outputFilePath, exportStatements.join('\n'), 'utf8')
+}
+
+prefixes.forEach((prefix) => {
+  processCssFileByTokenPrefix(TOKENS_DIR, prefix)
+})
+
+generateIndexFile()
