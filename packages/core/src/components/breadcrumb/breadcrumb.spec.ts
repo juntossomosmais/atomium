128cf038b6ffc69368bacd763a4b79f70f002f65
denilsonrp <denilsonraimundo17@gmail.com>
2023-06-28 11:22:13 -0300

    feat(breadcrumb): init new component

---
+++ packages/core/src/components/breadcrumb/breadcrumb.spec.ts
@@ -0,0 +1,54 @@
+import { newSpecPage } from '@stencil/core/testing'
+
+import { AtomBreadcrumb } from './breadcrumb'
+
+const breadcrumbItemsMock = [
+  {
+    text: 'First level',
+    title: 'Go to first level',
+    redirect: () => console.log('/first'),
+  },
+  {
+    text: 'Intermediary level',
+    title: 'Go to intermediary level',
+    redirect: () => console.log('/intermediary'),
+  },
+  {
+    text: 'Current level',
+    title: 'This is the current level',
+  },
+]
+
+describe('atom-breadcrumb', () => {
+  it('should render breadcrumb items correctly', async () => {
+    const page = await newSpecPage({
+      components: [AtomBreadcrumb],
+      html: `<atom-breadcrumb />`,
+    })
+
+    page.rootInstance.items = breadcrumbItemsMock
+
+    await page.waitForChanges()
+
+    expect(page.root).toEqualHtml(`
+      <atom-breadcrumb>
+        <mock:shadow-root>
+          <ion-breadcrumbs>
+            <ion-breadcrumb class="atom-breadcrumb" title="Go to first level">
+              First level
+              <atom-icon icon="caret-forward-outline" slot="separator"></atom-icon>
+            </ion-breadcrumb>
+            <ion-breadcrumb class="atom-breadcrumb" title="Go to intermediary level">
+              Intermediary level
+              <atom-icon icon="caret-forward-outline" slot="separator"></atom-icon>
+            </ion-breadcrumb>
+            <ion-breadcrumb class="atom-breadcrumb" title="This is the current level">
+              Current level
+              <atom-icon icon="caret-forward-outline" slot="separator"></atom-icon>
+            </ion-breadcrumb>
+          </ion-breadcrumbs>
+        </mock:shadow-root>
+      </atom-breadcrumb>
+    `)
+  })
+})
---
+++ packages/core/src/components/breadcrumb/breadcrumb.tsx
@@ -0,0 +1,31 @@
+import { Component, h, Host, Prop } from '@stencil/core'
+
+type BreadcrumbItemProp = {
+  redirect?: (event: MouseEvent) => void
+  text: string
+  title: string
+}
+
+@Component({
+  tag: 'atom-breadcrumb',
+  styleUrl: 'breadcrumb.scss',
+  shadow: true,
+})
+export class AtomBreadcrumb {
+  @Prop({ mutable: true }) items: BreadcrumbItemProp[] = []
+
+  render() {
+    return (
+      <Host>
+        <ion-breadcrumbs>
+          {this.items.map((item) => (
+            <ion-breadcrumb class="atom-breadcrumb" onClick={item.redirect}>
+              <span title={item.title}>{item.text}</span>
+              <atom-icon icon="caret-forward-outline" slot="separator" />
+            </ion-breadcrumb>
+          ))}
+        </ion-breadcrumbs>
+      </Host>
+    )
+  }
+}
