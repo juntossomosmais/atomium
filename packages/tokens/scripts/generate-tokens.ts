b3f5f8ad90ede2db7b43dd2331536d65769cad46
RodrigoRVSN <rodrigovictor81@gmail.com>
2023-06-29 10:59:18 -0300

    feat(tokens): change script to generate a single file with all tokens instead a lot of files

--- packages/tokens/scripts/generate-tokens.ts
+++ packages/tokens/scripts/generate-tokens.ts
@@ -4,15 +4,13 @@ import { fileURLToPath } from 'url'
 
 const CURRENT_DIR = path.dirname(fileURLToPath(import.meta.url))
 const TOKENS_DIR = path.resolve(CURRENT_DIR, '../tokens.css')
-const OUTPUT_DIR = path.resolve(CURRENT_DIR, '../../src')
-const OUTPUT_TOKENS_DIR = `${OUTPUT_DIR}/tokens`
+const OUTPUT_DIR = path.resolve(CURRENT_DIR, '../../')
 
 const prefixes = ['color', 'spacing', 'screen']
-const exportStatements: Array<string> = []
+const tokens = []
 
 function extractTokensFromCss(cssContent: string, prefix: string) {
   const cssVariablePattern = new RegExp(`--(${prefix}[\\w-]+):\\s*([^;]+)`, 'g')
-  const tokens = {}
 
   cssContent.replace(
     cssVariablePattern,
@@ -28,14 +26,9 @@ function extractTokensFromCss(cssContent: string, prefix: string) {
       return ''
     }
   )
-
-  return tokens
 }
 
-function generateJavaScriptFile(
-  tokens: { [token: string]: string },
-  outputFilePath: string
-) {
+function generateJavaScriptFile(outputFilePath: string) {
   const jsCode = Object.entries(tokens)
     .map(([variable, value]) => `export const ${variable} = '${value}';`)
     .join('\n')
@@ -43,29 +36,15 @@ function generateJavaScriptFile(
   fs.writeFileSync(outputFilePath, jsCode, 'utf8')
 }
 
-function processCssFileByTokenPrefix(cssFilePath: string, prefix: string) {
+function processCssFileByTokenPrefix(cssFilePath: string) {
   const cssContent = fs.readFileSync(cssFilePath, 'utf8')
-  const tokens = extractTokensFromCss(cssContent, prefix)
 
-  const outputFileName = `${prefix}.ts`
-  const outputFilePath = path.join(OUTPUT_TOKENS_DIR, outputFileName)
+  prefixes.forEach((prefix) => extractTokensFromCss(cssContent, prefix))
 
-  generateJavaScriptFile(tokens, outputFilePath)
-  exportStatements.push(`export * from './${prefix}';`)
-}
-
-function generateIndexFile() {
   const outputFileName = 'index.ts'
-  const outputFilePath = path.join(OUTPUT_TOKENS_DIR, outputFileName)
-  fs.writeFileSync(outputFilePath, exportStatements.join('\n'), 'utf8')
+  const outputFilePath = path.join(OUTPUT_DIR, outputFileName)
+
+  generateJavaScriptFile(outputFilePath)
 }
 
-if (!fs.existsSync(OUTPUT_TOKENS_DIR)) {
-  fs.mkdirSync(OUTPUT_TOKENS_DIR)
-}
-
-prefixes.forEach((prefix) => {
-  processCssFileByTokenPrefix(TOKENS_DIR, prefix)
-})
-
-generateIndexFile()
+processCssFileByTokenPrefix(TOKENS_DIR)
