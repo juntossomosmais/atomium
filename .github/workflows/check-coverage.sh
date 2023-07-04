8d3461052d1c385fca9414fd7e32d889b529a2ec
Tássio <tassio72@hotmail.com>
2023-07-04 13:34:01 -0300

    ci: use sh instead

---
+++ .github/workflows/check-coverage.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+directory=$1
+
+if [ -d "$directory/coverage" ]; then
+  echo "Coverage directory exists for $directory."
+  echo "COVERAGE_$directory=true" >> $GITHUB_ENV
+else
+  echo "Coverage directory does not exist for $directory."
+  echo "COVERAGE_$directory=false" >> $GITHUB_ENV
+fi
