542876e980a4515ccee736f57e8a61717bfbbbc2
Tássio <tassio72@hotmail.com>
2023-07-04 14:14:29 -0300

    ci: use packages prefix

--- .github/workflows/check-coverage.sh
+++ .github/workflows/check-coverage.sh
@@ -2,10 +2,12 @@
 
 directory=$1
 
+directory_upper=$(echo "$directory" | tr '[:lower:]' '[:upper:]')
+
 if [ -d "packages/$directory/coverage" ]; then
   echo "Coverage directory exists for $directory."
-  echo "COVERAGE_$directory=true" >> $GITHUB_ENV
+  echo "COVERAGE_$directory_upper=true" >> $GITHUB_ENV
 else
   echo "Coverage directory does not exist for $directory."
-  echo "COVERAGE_$directory=false" >> $GITHUB_ENV
+  echo "COVERAGE_$directory_upper=false" >> $GITHUB_ENV
 fi
