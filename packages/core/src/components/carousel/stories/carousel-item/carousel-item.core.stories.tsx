ce0471f4b70416cbab9da4dc6d75a8bf9f55af13
Alan Schio <alanbschio@gmail.com>
2023-08-08 18:45:24 -0300

    feat(CarouselItem): creating a carousel item in order to can be used with jsx

--- packages/core/src/components/carousel/stories/carousel-item/carousel-item.core.stories.tsx
+++ packages/core/src/components/carousel/stories/carousel-item/carousel-item.core.stories.tsx
@@ -4,14 +4,28 @@ import { CarouselItemStoryArgs } from './carouse-item.args'
 export default {
   title: 'Components/Carousel Item',
   ...CarouselItemStoryArgs,
-  withSource: false,
 } as Meta
 
 export const Default: StoryObj = {
   render: () =>
-    `&lt;atom-carousel-item&gt;
-      &lt;div class="item"&gt;Slide 1&lt;/div&gt;
-    &lt;/atom-carousel-item&gt;
+    `<pre>
+&lt;atom-carousel-item&gt;
+    &lt;div class="item"&gt;Slide 1&lt;/div&gt;
+&lt;/atom-carousel-item&gt;
+    </pre>
     `,
 }
-//
+export const Lazy: StoryObj = {
+  render: () =>
+    `<pre>
+  &lt;atom-carousel-item&gt;
+    &lt;div class="item"&gt;
+      &lt;img
+      loading="lazy"
+        width="100%"
+        src="https://user-images.githubusercontent.com/3603793/257943112-fb180815-7bd7-45f7-ad14-bd1677079931.png"
+      /&gt;
+  &lt;/div&gt;
+&lt;/atom-carousel-item&gt;
+</pre>`,
+}
---
+++ packages/core/src/components/carousel/stories/carousel-item/carousel-item.react.stories.tsx
@@ -0,0 +1,31 @@
+import { Meta, StoryObj } from '@storybook/react'
+import React from 'react'
+import { CarouselItemStoryArgs } from './carouse-item.args'
+
+import { AtomCarouselItem } from '@juntossomosmais/atomium/react'
+export default {
+  title: 'Components/Carousel Item',
+  ...CarouselItemStoryArgs,
+} as Meta
+
+export const Default: StoryObj = {
+  render: () => (
+    <AtomCarouselItem>
+      <div>Slide 1</div>
+    </AtomCarouselItem>
+  ),
+}
+
+export const Lazy: StoryObj = {
+  render: () => (
+    <AtomCarouselItem lazy={true}>
+      <div>
+        <img
+          loading="lazy"
+          width="100%"
+          src="https://user-images.githubusercontent.com/3603793/257943112-fb180815-7bd7-45f7-ad14-bd1677079931.png"
+        />
+      </div>
+    </AtomCarouselItem>
+  ),
+}
