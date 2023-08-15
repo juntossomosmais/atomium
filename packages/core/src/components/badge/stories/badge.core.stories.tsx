a9bae6cb45c8e33d565701d842391d56df865131
Gabriel <gabrielmonteiroduete@gmail.com>
2023-08-15 19:25:40 -0300

    feat: create stories

---
+++ packages/core/src/components/badge/stories/badge.core.stories.tsx
@@ -0,0 +1,78 @@
+import { Meta, StoryObj } from '@storybook/web-components'
+
+import { html } from 'lit'
+
+import { BadgeStoryArgs } from './badge.args'
+
+export default {
+  title: 'Components/Badge',
+  ...BadgeStoryArgs,
+} as Meta
+
+const createBadge = (args) => {
+  return html` <atom-badge type="${args.type}"> ${args.children} </atom-badge> `
+}
+
+export const Primary: StoryObj = {
+  render: (args) => createBadge(args),
+  args: {
+    type: 'primary',
+    children: 'Badge',
+  },
+}
+
+export const Secondary: StoryObj = {
+  render: (args) => createBadge(args),
+  args: {
+    ...Primary.args,
+    type: 'secondary',
+  },
+}
+
+export const Success: StoryObj = {
+  render: (args) => createBadge(args),
+  args: {
+    ...Primary.args,
+    type: 'success',
+  },
+}
+
+export const Warning: StoryObj = {
+  render: (args) => createBadge(args),
+  args: {
+    ...Primary.args,
+    type: 'warming',
+  },
+}
+
+export const Danger: StoryObj = {
+  render: (args) => createBadge(args),
+  args: {
+    ...Primary.args,
+    type: 'danger',
+  },
+}
+
+export const Light: StoryObj = {
+  render: (args) => createBadge(args),
+  args: {
+    ...Primary.args,
+    type: 'light',
+  },
+}
+
+export const Dark: StoryObj = {
+  render: (args) => createBadge(args),
+  args: {
+    ...Primary.args,
+    type: 'dark',
+  },
+}
+
+export const Purple: StoryObj = {
+  render: (args) => createBadge(args),
+  args: {
+    ...Primary.args,
+    type: 'purple',
+  },
+}
---
+++ packages/core/src/components/badge/stories/badge.react.stories.tsx
@@ -0,0 +1,80 @@
+import { Meta, StoryObj } from '@storybook/react'
+import React from 'react'
+
+import { AtomBadge } from '@juntossomosmais/atomium/react'
+
+import { BadgeStoryArgs } from './badge.args'
+
+export default {
+  title: 'Components/Button',
+  component: AtomBadge,
+  ...BadgeStoryArgs,
+} as Meta
+
+const createBadge = (args) => (
+  <AtomBadge color={args.color}>{args.label}</AtomBadge>
+)
+
+export const Primary: StoryObj = {
+  render: (args) => createBadge(args),
+  args: {
+    type: 'primary',
+    label: 'Badge',
+  },
+}
+
+export const Secondary: StoryObj = {
+  render: (args) => createBadge(args),
+  args: {
+    ...Primary.args,
+    type: 'secondary',
+  },
+}
+
+export const Success: StoryObj = {
+  render: (args) => createBadge(args),
+  args: {
+    ...Primary.args,
+    type: 'success',
+  },
+}
+
+export const Warning: StoryObj = {
+  render: (args) => createBadge(args),
+  args: {
+    ...Primary.args,
+    type: 'warming',
+  },
+}
+
+export const Danger: StoryObj = {
+  render: (args) => createBadge(args),
+  args: {
+    ...Primary.args,
+    type: 'danger',
+  },
+}
+
+export const Light: StoryObj = {
+  render: (args) => createBadge(args),
+  args: {
+    ...Primary.args,
+    type: 'light',
+  },
+}
+
+export const Dark: StoryObj = {
+  render: (args) => createBadge(args),
+  args: {
+    ...Primary.args,
+    type: 'dark',
+  },
+}
+
+export const Purple: StoryObj = {
+  render: (args) => createBadge(args),
+  args: {
+    ...Primary.args,
+    type: 'purple',
+  },
+}
