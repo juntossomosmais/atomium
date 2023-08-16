c3c8856cb0f1ad50cd74c5e03196495a2e01a76c
Gabriel <gabrielmonteiroduete@gmail.com>
2023-08-16 17:14:32 -0300

    test: add tests badge component

--- packages/core/src/components/badge/badge.spec.ts
+++ packages/core/src/components/badge/badge.spec.ts
@@ -1,3 +1,38 @@
-describe('AtomBadge', () => {
-  it('should render an ion-badge element', async () => {})
+import { newSpecPage } from '@stencil/core/testing'
+
+import { AtomBadge } from './badge'
+
+describe('atom-badge', () => {
+  it('should render an ion-badge element correctly', async () => {
+    const page = await newSpecPage({
+      components: [AtomBadge],
+      html: `<atom-badge type='primary'>Badge</atom-badge>`,
+    })
+
+    await page.waitForChanges()
+
+    const badgeEl = page.root?.shadowRoot?.querySelector('ion-badge')
+
+    expect(badgeEl).toBeTruthy()
+  })
+
+  it('should render badge element correctly', async () => {
+    const page = await newSpecPage({
+      components: [AtomBadge],
+      html: `<atom-badge type='primary'>Badge</atom-badge>`,
+    })
+
+    await page.waitForChanges()
+
+    expect(page.root).toEqualHtml(`
+      <atom-badge type='primary'>
+        <mock:shadow-root>
+        <ion-badge color='primary' class="atom-badge">
+          <slot></slot>
+        </ion-badge>
+        </mock:shadow-root>
+        Badge
+      </atom-badge>
+    `)
+  })
 })
