b50ff413c8db52091be1a99288e70acf52ed38e2
Gabriel Duete <gabrielmonteiroduete@gmail.com>
2023-08-24 15:14:49 -0300

    feat: create badge component (#239)

---
+++ packages/core/src/components/badge/badge.tsx
@@ -0,0 +1,26 @@
+import { Component, Prop, h } from '@stencil/core'
+
+@Component({
+  tag: 'atom-badge',
+  styleUrl: 'badge.scss',
+  shadow: true,
+})
+export class AtomBadge {
+  @Prop() type:
+    | 'primary'
+    | 'secondary'
+    | 'info'
+    | 'success'
+    | 'warning'
+    | 'danger'
+    | 'neutral'
+    | 'dark' = 'primary'
+
+  render() {
+    return (
+      <ion-badge color={this.type} class="atom-badge">
+        <slot />
+      </ion-badge>
+    )
+  }
+}
