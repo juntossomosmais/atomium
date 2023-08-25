3d332974b7aa08e00e3db533a7eabe2280c53a61
Gabriel <gabrielmonteiroduete@gmail.com>
2023-08-25 17:12:13 -0300

    fix: use lineheight

--- packages/core/src/components/badge/badge.tsx
+++ packages/core/src/components/badge/badge.tsx
@@ -1,4 +1,4 @@
-import { Component, Prop, h } from '@stencil/core'
+import { Component, Host, Prop, h } from '@stencil/core'
 
 @Component({
   tag: 'atom-badge',
@@ -18,9 +18,11 @@ export class AtomBadge {
 
   render() {
     return (
-      <ion-badge color={this.type} class="atom-badge">
-        <slot />
-      </ion-badge>
+      <Host>
+        <ion-badge color={this.type} class="atom-badge">
+          <slot />
+        </ion-badge>
+      </Host>
     )
   }
 }
