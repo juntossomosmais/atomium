f7cdf1011097b0bedf38c6001df7048824eb16c9
luiz-ortega <luizngust@gmail.com>
2023-09-13 13:13:51 -0300

    feat: add atom-tag component

---
+++ packages/core/src/components/tag/tag.tsx
@@ -0,0 +1,25 @@
+import { Component, Host, Prop, h } from '@stencil/core'
+import { IconProps } from '../../icons'
+
+@Component({
+  tag: 'atom-tag',
+  styleUrl: 'tag.scss',
+  shadow: true,
+})
+export class AtomTag {
+  @Prop() type: 'success' | 'danger' | 'warning' | 'dark' | 'light' | 'neutral'
+  @Prop() icon?: IconProps
+
+  render() {
+    return (
+      <Host>
+        <ion-badge color={this.type} class='atom-tag'>
+          {this.icon && (
+            <atom-icon class='atom-icon' icon={this.icon}></atom-icon>
+          )}
+          <slot />
+        </ion-badge>
+      </Host>
+    )
+  }
+}
