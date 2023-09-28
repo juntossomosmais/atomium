e98b6585cf6d462fe8b382286575081a408d757a
Felipe Fialho <hi@felipefialho.com>
2023-09-28 10:36:27 -0300

    chore: extends eslint from @juntossomosmais/linters (#269)

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+const baseConfig = require('@juntossomosmais/linters/eslint.config.js')
+
+module.exports = {
+  ...baseConfig,
+  root: true,
+  settings: {
+    'import/resolver': {
+      typescript: {
+        project: './tsconfig.base.json',
+      },
+    },
+  },
+  overrides: [
+    ...baseConfig.overrides,
+    {
+      files: ['**/*.spec.{js,ts,tsx}'],
+      rules: {
+        'sonarjs/no-duplicate-string': 'off',
+      },
+    },
+    {
+      files: ['**/scripts/**'],
+      rules: {
+        'no-console': 'off',
+      },
+    },
+  ],
+}
