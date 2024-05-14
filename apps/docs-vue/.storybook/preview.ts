5aea393d6e68de63f4ace110575a362b2d52d9e4
felipefialho <hi@felipefialho.com>
2024-05-14 14:52:36 -0300

    feat: add storybook for vue

---
+++ apps/docs-vue/.storybook/preview.ts
@@ -0,0 +1,21 @@
+import { CustomViewports } from '@atomium/storybook-utils/custom-viewports'
+import DocumentationTemplate from '@atomium/storybook-utils/DocumentationTemplate.mdx'
+
+import '@atomium/storybook-utils/preview.css'
+
+import '@juntossomosmais/atomium-tokens/tokens.css'
+import '@juntossomosmais/atomium/dist/core/core.css'
+
+const preview = {
+  parameters: {
+    viewport: {
+      viewports: CustomViewports,
+    },
+    actions: { argTypesRegex: '^atom.*' },
+    docs: {
+      page: DocumentationTemplate,
+    },
+  },
+}
+
+export default preview
