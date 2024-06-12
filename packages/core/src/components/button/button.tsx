f39f99f3fc2d6c1c6158cd3266c4b679a5c81b14
felipefialho <hi@felipefialho.com>
2024-06-12 11:11:39 -0300

    chore(core): avoid warning make props mutable

--- packages/core/src/components/button/button.tsx
+++ packages/core/src/components/button/button.tsx
@@ -15,19 +15,19 @@ import {
   shadow: true,
 })
 export class AtomButton {
-  @Prop() color: 'primary' | 'secondary' | 'white' = 'primary'
-  @Prop() disabled?: boolean
-  @Prop() download?: string
-  @Prop() expand?: 'block'
-  @Prop() fill: 'clear' | 'outline' | 'solid' = 'solid'
-  @Prop() shape?: 'round' | 'circle' = 'round'
-  @Prop() href?: string
-  @Prop() loading?: boolean
-  @Prop() mode: Mode = 'md'
-  @Prop() rel?: string
-  @Prop() size: 'small' | 'default' | 'large' = 'default'
-  @Prop() target?: string
-  @Prop() type: 'submit' | 'reset' | 'button' = 'button'
+  @Prop({ mutable: true }) color: 'primary' | 'secondary' | 'white' = 'primary'
+  @Prop({ mutable: true }) disabled?: boolean
+  @Prop({ mutable: true }) download?: string
+  @Prop({ mutable: true }) expand?: 'block'
+  @Prop({ mutable: true }) fill: 'clear' | 'outline' | 'solid' = 'solid'
+  @Prop({ mutable: true }) shape?: 'round' | 'circle' = 'round'
+  @Prop({ mutable: true }) href?: string
+  @Prop({ mutable: true }) loading?: boolean
+  @Prop({ mutable: true }) mode: Mode = 'md'
+  @Prop({ mutable: true }) rel?: string
+  @Prop({ mutable: true }) size: 'small' | 'default' | 'large' = 'default'
+  @Prop({ mutable: true }) target?: string
+  @Prop({ mutable: true }) type: 'submit' | 'reset' | 'button' = 'button'
 
   @Event() click: EventEmitter
 
