3265ed0f910bc71b0d40c8d20543be04993dcb22
felipefialho <hi@felipefialho.com>
2024-06-12 19:55:08 -0300

    test: add new tests

--- packages/core/src/components/list-slider/list-slider.spec.ts
+++ packages/core/src/components/list-slider/list-slider.spec.ts
@@ -4,45 +4,29 @@ import { AtomListSlider } from './list-slider'
 import { AtomListSliderItem } from './list-slider-item/list-slider-item'
 
 describe('AtomListSlider', () => {
-  let component: AtomListSlider
-
   beforeEach(() => {
     global.ResizeObserver = class ResizeObserver {
       observe() {}
       unobserve() {}
       disconnect() {}
     }
-    component = new AtomListSlider()
-    const items = Array(5)
-      .fill(null)
-      .map((_, index) => {
-        const item = document.createElement('div')
-
-        Object.defineProperty(item, 'offsetWidth', {
-          value: 100 + 10 * index,
-        })
-
-        return item
-      })
-
-    component.sliderItems = items as unknown as NodeListOf<HTMLElement>
-    component.sliderGapValue = 10
-    component.viewportWidth = 400
   })
 
-  it('should render an atom-list-slider and atom-list-slider-item elements', async () => {
+  it('should remove navigation buttons when hasNavigation is false', async () => {
     const page = await newSpecPage({
       components: [AtomListSlider, AtomListSliderItem],
       html: `
-        <atom-list-slider>
+        <atom-list-slider has-navigation="false">
           <atom-list-slider-item>Slide 1</atom-list-slider-item>
           <atom-list-slider-item>Slide 2</atom-list-slider-item>
         </atom-list-slider>
       `,
     })
 
+    await page.waitForChanges()
+
     expect(page?.root).toEqualHtml(`
-      <atom-list-slider>
+      <atom-list-slider has-navigation="false">
         <mock:shadow-root>
           <div aria-label="Carousel" class="atom-list-slider" role="region">
             <button aria-disabled="true" aria-label="Previous" class="navigation navigation--prev" role="button">
@@ -53,7 +37,7 @@ describe('AtomListSlider', () => {
                 <slot></slot>
               </div>
             </div>
-            <button aria-label="Next" class="navigation navigation--next" role="button">
+            <button aria-disabled="true" aria-label="Next" class="navigation navigation--next" role="button">
               <atom-icon icon="chevron-right"></atom-icon>
             </button>
           </div>
@@ -74,46 +58,44 @@ describe('AtomListSlider', () => {
     `)
   })
 
-  it('should remove navigation buttons when hasNavigation is false', async () => {
+  it('should centralize the slider when centralized is true', async () => {
     const page = await newSpecPage({
       components: [AtomListSlider, AtomListSliderItem],
       html: `
-        <atom-list-slider has-navigation="false">
-          <atom-list-slider-item>Slide 1</atom-list-slider-item>
-          <atom-list-slider-item>Slide 2</atom-list-slider-item>
+        <atom-list-slider centralized="true">
+          <atom-list-slider-item style="width: 100px">Slide 1</atom-list-slider-item>
         </atom-list-slider>
       `,
     })
 
+    page.rootInstance.viewportWidth = 1000
+
+    await page.waitForChanges()
+
     expect(page?.root).toEqualHtml(`
-      <atom-list-slider has-navigation="false">
+      <atom-list-slider centralized="true">
         <mock:shadow-root>
           <div aria-label="Carousel" class="atom-list-slider" role="region">
+            <button aria-disabled="true" aria-label="Previous" class="navigation navigation--prev" role="button">
+              <atom-icon icon="chevron-left"></atom-icon>
+            </button>
             <div aria-live="polite" class="sliders">
-              <div class="wrapper" role="list">
+              <div class="wrapper wrapper--centralized" role="list">
                 <slot></slot>
               </div>
             </div>
+            <button aria-disabled="true" aria-label="Next" class="navigation navigation--next" role="button">
+              <atom-icon icon="chevron-right"></atom-icon>
+            </button>
           </div>
         </mock:shadow-root>
-        <atom-list-slider-item>
+        <atom-list-slider-item style="width: 100px;">
           <mock:shadow-root>
             <slot></slot>
           </mock:shadow-root>
           Slide 1
         </atom-list-slider-item>
-        <atom-list-slider-item>
-          <mock:shadow-root>
-            <slot></slot>
-          </mock:shadow-root>
-          Slide 2
-        </atom-list-slider-item>
       </atom-list-slider>
     `)
-  })
-
-  it('should correctly calculate maxTranslateX', () => {
-    component.handleMaxTranslateX()
-    expect(component.maxTranslateX).toBe(240)
   })
 })
