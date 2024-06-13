d76b113a89868623f8c5c07c331206686f05b7e4
felipefialho <felipefialho@users.noreply.github.com>
2024-06-13 13:29:44 +0000

    Remove preview for PR 462 🛬

--- pr-preview/pr-462/vue/assets/preview-DZLMSF1E.js
+++
@@ -1,2 +0,0 @@
-const __vite__fileDeps=["./DocsRenderer-NNNQARDV-Dr_HYnRy.js","./chunk-HLWAVYOI-B6vHBBYv.js","./iframe-BlaZyy6T.js","./index-C_ZXXQk-.js","./_commonjsHelpers-Cpj98o6Y.js","./doctrine-Z_8G-UDG.js","./index-DrFu-skq.js"],__vite__mapDeps=i=>i.map(i=>__vite__fileDeps[i]);
-import{_ as e}from"./iframe-BlaZyy6T.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-Dr_HYnRy.js"),__vite__mapDeps([0,1,2,3,4,5,6]),import.meta.url);return new r}}};export{a as parameters};
