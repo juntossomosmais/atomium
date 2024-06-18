0aae66a934dba49614ad104a8b6ec7354123cc83
felipefialho <felipefialho@users.noreply.github.com>
2024-06-18 15:18:29 +0000

    Deploy preview for PR 471 🛫

---
+++ pr-preview/pr-471/vue/assets/preview-C-cpNeb6.js
@@ -0,0 +1,2 @@
+const __vite__fileDeps=["./DocsRenderer-NNNQARDV-Bzo-SKls.js","./chunk-HLWAVYOI-C8S_3JJt.js","./iframe-Bz-JRNOn.js","./index-DR4CqWwU.js","./_commonjsHelpers-Cpj98o6Y.js","./doctrine-Z_8G-UDG.js","./index-DrFu-skq.js"],__vite__mapDeps=i=>i.map(i=>__vite__fileDeps[i]);
+import{_ as e}from"./iframe-Bz-JRNOn.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-Bzo-SKls.js"),__vite__mapDeps([0,1,2,3,4,5,6]),import.meta.url);return new r}}};export{a as parameters};
