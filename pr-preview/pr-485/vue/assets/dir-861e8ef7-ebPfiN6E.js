c0f360c805edeb4161cbe9947fda8a77e228237d
felipefialho <felipefialho@users.noreply.github.com>
2024-07-02 20:46:17 +0000

    Remove preview for PR 485 🛬

--- pr-preview/pr-485/vue/assets/dir-861e8ef7-ebPfiN6E.js
+++
@@ -1,3 +0,0 @@
-/*!
- * (C) Ionic http://ionicframework.com - MIT License
- */const e=r=>r&&r.dir!==""?r.dir.toLowerCase()==="rtl":(document==null?void 0:document.dir.toLowerCase())==="rtl";export{e as i};
