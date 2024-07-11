b188d08d41c6ded1ecbb99c17400a66fbe28455a
SallesCosta <SallesCosta@users.noreply.github.com>
2024-07-11 12:20:31 +0000

    Deploy preview for PR 487 🛫

---
+++ pr-preview/pr-487/vue/assets/menu-toggle-util-7e25654d-DONNsr_R.js
@@ -0,0 +1,3 @@
+import{m as a}from"./index-9a646cc4-BRSScA3D.js";/*!
+ * (C) Ionic http://ionicframework.com - MIT License
+ */const n=async i=>{const t=await a.get(i);return!!(t&&await t.isActive())};export{n as u};
