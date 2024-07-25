be732ea330d5f84da0cf829aa3c151c0578b737d
igorwessel <igorwessel@users.noreply.github.com>
2024-07-25 16:59:51 +0000

    Remove preview for PR 500 🛬

--- pr-preview/pr-500/vue/assets/preview-DEa2Wa-2.js
+++
@@ -1,2 +0,0 @@
-const __vite__fileDeps=["./DocsRenderer-NNNQARDV-mMo6Pjiz.js","./chunk-HLWAVYOI-CXEjTNez.js","./iframe-CsQmtAh_.js","./index-D4n3R6NP.js","./_commonjsHelpers-Cpj98o6Y.js","./doctrine-Z_8G-UDG.js","./index-DrFu-skq.js"],__vite__mapDeps=i=>i.map(i=>__vite__fileDeps[i]);
-import{_ as e}from"./iframe-CsQmtAh_.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-mMo6Pjiz.js"),__vite__mapDeps([0,1,2,3,4,5,6]),import.meta.url);return new r}}};export{a as parameters};
