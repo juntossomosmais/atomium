356fe0c6e1ad0f39c4e1ea7c6cf344a1aa0dd21b
Tássio <tassio72@hotmail.com>
2024-07-30 15:52:46 -0300

    feat(component): create link component (#502)

---
+++ packages/core/src/components/link/stories/link.args.ts
@@ -0,0 +1,47 @@
+export const LinkStoryArgs = {
+  decorators: [],
+  parameters: {
+    actions: {
+      handles: [],
+    },
+    docs: {
+      description: {
+        component:
+          'atom-link components are link children styled components. They are used to navigate to different pages (when used inside router components, such as router-link(Vue) and Link(Next)) or used to trigger user actions.',
+      },
+    },
+  },
+  argTypes: {
+    color: {
+      control: 'select',
+      options: ['primary', 'secondary'],
+      defaultValue: { summary: 'primary' },
+      description: 'The link color.',
+    },
+    type: {
+      control: 'select',
+      options: ['anchor', 'button'],
+      defaultValue: { summary: 'anchor' },
+      description:
+        'The atom-link type. Use anchor for navigation (combined with router-link or Link) and button for user actions.',
+    },
+  },
+}
+
+const LinkReactStoryArgs = JSON.parse(JSON.stringify(LinkStoryArgs))
+
+LinkReactStoryArgs.parameters.docs.description.component =
+  'atom-link components are link children styled components. They are used to navigate to different pages (when used inside Link(Next)) or used to trigger user actions.<br/><br/> OBS: Link (Next) component does not render a anchor tag by default, so you need to wrap it with a tag for semantic reasons. You can create a wrapper component on your project to do this.'
+
+LinkReactStoryArgs.argTypes.type.description =
+  'The atom-link type. Use anchor for navigation (combined with Link) and button for user actions.'
+
+const LinkVueStoryArgs = JSON.parse(JSON.stringify(LinkStoryArgs))
+
+LinkVueStoryArgs.parameters.docs.description.component =
+  'atom-link components are link children styled components. They are used to navigate to different pages (when used inside router-link or NuxtLink or used to trigger user actions.'
+
+LinkVueStoryArgs.argTypes.type.description =
+  'The atom-link type. Use anchor for navigation (combined with router-link or NuxtLink) and button for user actions.'
+
+export { LinkReactStoryArgs, LinkVueStoryArgs }
---
+++ packages/core/src/components/link/stories/link.core.stories.tsx
@@ -0,0 +1,65 @@
+import { Meta, StoryObj } from '@storybook/web-components'
+import { html } from 'lit'
+
+import { LinkStoryArgs } from './link.args'
+
+export default {
+  title: 'Components/Link',
+  ...LinkStoryArgs,
+} as Meta
+
+const createLink = (
+  args,
+  textExample = 'It should be used inside router components'
+) => {
+  return html`
+    <a href="/nice-example">
+      <atom-link type="${args.type}" color="${args.color}">
+        ${textExample}
+      </atom-link>
+    </a>
+  `
+}
+
+export const Primary: StoryObj = {
+  render: (args) => createLink(args),
+  args: {
+    type: 'anchor',
+    color: 'primary',
+  },
+}
+
+export const Secondary: StoryObj = {
+  render: (args) => createLink(args),
+  args: {
+    ...Primary.args,
+    color: 'secondary',
+  },
+}
+
+export const WithIcon: StoryObj = {
+  render: (args) => html`
+    <a href="/nice-example">
+      <atom-link type="${args.type}" color="${args.color}">
+        <span> Nice example with icon </span>
+        <atom-icon icon="heart" />
+      </atom-link>
+    </a>
+  `,
+  args: {
+    ...Primary.args,
+    color: 'secondary',
+  },
+}
+
+export const Button: StoryObj = {
+  render: (args) => html`
+    <atom-link type="${args.type}" color="${args.color}">
+      It is a button! and can be used to trigger user actions
+    </atom-link>
+  `,
+  args: {
+    ...Primary.args,
+    type: 'button',
+  },
+}
