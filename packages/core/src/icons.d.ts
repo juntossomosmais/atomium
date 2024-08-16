f04d310243aae09c2465aae04d10875dc7c4dfc7
Matheus Rosa <106702647+matheus-rosa-jsm@users.noreply.github.com>
2024-08-16 15:07:51 -0300

    feat(icons): add verified icons (#539)

--- packages/core/src/icons.d.ts
+++ packages/core/src/icons.d.ts
@@ -175,6 +175,8 @@ export type IconProps =
   | 'tune'
   | 'upload-outline'
   | 'upload'
+  | 'verified-outiline'
+  | 'verified'
   | 'wallet-giftcard'
   | 'watch-later-outline'
   | 'watch-later'
