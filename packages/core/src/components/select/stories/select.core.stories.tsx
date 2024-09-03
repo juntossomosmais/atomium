373e09dd5e2323ed5811788ba437016cd7fa8ccf
Natalie9 <natalietolentino9@gmail.com>
2024-09-03 14:55:55 -0300

    feat: add example with tag

--- packages/core/src/components/select/stories/select.core.stories.tsx
+++ packages/core/src/components/select/stories/select.core.stories.tsx
@@ -8,7 +8,24 @@ export default {
   ...SelectStoryArgs,
 } as Meta
 
-const createSelect = (args) => {
+const optionsDefault = [
+  { id: '1', value: 'Red', disabled: false },
+  {
+    id: '2',
+    value: 'Green',
+    disabled: false,
+  },
+  { id: '3', value: 'Blue', disabled: false },
+  {
+    id: '4',
+    value: 'nice_blue',
+    disabled: false,
+    label: 'Nice Blue',
+  },
+  { id: '5', value: 'Disabled example', disabled: true },
+]
+
+const createSelect = (args, options = optionsDefault) => {
   return html`
     <atom-select
       placeholder=${args.placeholder}
@@ -28,30 +45,12 @@ const createSelect = (args) => {
     <script>
       ;(function () {
         const atomSelectElements = document.querySelectorAll('atom-select')
+        const lastElement = atomSelectElements[atomSelectElements.length - 1]
 
-        atomSelectElements.forEach((atomSelect) => {
-          atomSelect.options = [
-            { id: '1', value: 'Red', disabled: false },
-            {
-              id: '2',
-              value: 'Green',
-              disabled: false,
-              tag: { color: 'success', label: 'New' },
-            },
-            { id: '3', value: 'Blue', disabled: false },
-            {
-              id: '4',
-              value: 'nice_blue',
-              disabled: false,
-              label: 'Nice Blue',
-              tag: { color: 'success', label: 'New' },
-            },
-            { id: '5', value: 'Disabled example', disabled: true },
-          ]
+        lastElement.options = ${JSON.stringify(options)}
 
-          atomSelect.addEventListener('atomChange', (event) => {
-            console.log('atomChange', event)
-          })
+        lastElement.addEventListener('atomChange', (event) => {
+          console.log('atomChange', event)
         })
       })()
     </script>
@@ -102,5 +101,22 @@ export const Multiple: StoryObj = {
   args: {
     ...SelectComponentArgs,
     multiple: true,
+  },
+}
+
+const optionWhitTag = [
+  ...optionsDefault,
+  {
+    id: '3',
+    value: 'Nice Green',
+    disabled: false,
+    tag: { color: 'success', label: 'New ' },
+  },
+]
+
+export const WhitTag: StoryObj = {
+  render: (args) => createSelect(args, optionWhitTag),
+  args: {
+    ...SelectComponentArgs,
   },
 }
