818feb126e5716489058eb18263c4e1580c8fc52
matheus-rosa-jsm <matheus-rosa-jsm@users.noreply.github.com>
2024-09-05 12:08:38 +0000

    Deploy preview for PR 562 🛫

---
+++ pr-preview/pr-562/vue/assets/preview-CXPBT9B8.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./DocsRenderer-NNNQARDV-Bkrnchcq.js","./chunk-HLWAVYOI-DLeLSZAx.js","./iframe-CgGgLGa7.js","./index-Blxo39wZ.js","./_commonjsHelpers-Cpj98o6Y.js","./doctrine-Z_8G-UDG.js","./index-DrFu-skq.js"])))=>i.map(i=>d[i]);
+import{_ as e}from"./iframe-CgGgLGa7.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-Bkrnchcq.js"),__vite__mapDeps([0,1,2,3,4,5,6]),import.meta.url);return new r}}};export{a as parameters};
