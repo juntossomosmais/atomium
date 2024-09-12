e30709fa518f8b1d72463b77d7ce9fe2f4288af0
felipefialho <felipefialho@users.noreply.github.com>
2024-09-12 18:33:53 +0000

    Remove preview for PR 570 🛬

--- pr-preview/pr-570/vue/assets/syntaxhighlighter-B5GMVT5T-DxGMt3dK.js
+++
@@ -1,1 +0,0 @@
-import{S as e,c as s,s as c}from"./index-DFbV9MSc.js";import"./iframe-BnxR8lTl.js";import"../sb-preview/runtime.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./doctrine-Z_8G-UDG.js";import"./index-DrFu-skq.js";export{e as SyntaxHighlighter,s as createCopyToClipboardFunction,c as default};
