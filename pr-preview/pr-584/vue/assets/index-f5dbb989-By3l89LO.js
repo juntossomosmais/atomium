4dd077dd3f2f751ff2299829e6e69a4f21907a42
tiago-dela-rosa <tiago-dela-rosa@users.noreply.github.com>
2024-10-01 14:21:02 +0000

    Deploy preview for PR 584 🛫

---
+++ pr-preview/pr-584/vue/assets/index-f5dbb989-By3l89LO.js
@@ -0,0 +1,3 @@
+/*!
+ * (C) Ionic http://ionicframework.com - MIT License
+ */const o=(r,...n)=>console.warn(`[Ionic Warning]: ${r}`,...n),e=(r,...n)=>console.error(`[Ionic Error]: ${r}`,...n),t=(r,...n)=>console.error(`<${r.tagName.toLowerCase()}> must be used inside ${n.join(" or ")}.`);export{e as a,t as b,o as p};
