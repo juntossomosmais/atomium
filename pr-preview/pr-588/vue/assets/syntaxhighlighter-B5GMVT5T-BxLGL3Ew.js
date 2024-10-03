612dc4a059bf3fd7caed016c00279539375c792a
gabrielduete <gabrielduete@users.noreply.github.com>
2024-10-03 13:51:04 +0000

    Remove preview for PR 588 🛬

--- pr-preview/pr-588/vue/assets/syntaxhighlighter-B5GMVT5T-BxLGL3Ew.js
+++
@@ -1,1 +0,0 @@
-import{S as e,c as s,s as c}from"./index-Drg7tmQs.js";import"./iframe-BztUJW8Z.js";import"../sb-preview/runtime.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./doctrine-Z_8G-UDG.js";import"./index-DrFu-skq.js";export{e as SyntaxHighlighter,s as createCopyToClipboardFunction,c as default};
