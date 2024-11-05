0460663c39db268376223955662c6cb065e6b818
igorwessel <igorwessel@users.noreply.github.com>
2024-11-05 19:37:59 +0000

    Deploy preview for PR 367 🛫

---
+++ pr-preview/pr-367/vue/assets/form-controller-d1a439dc-yINrMp-s.js
@@ -0,0 +1,3 @@
+import{o}from"./helpers-079c01eb-D592YRe8.js";/*!
+ * (C) Ionic http://ionicframework.com - MIT License
+ */const O=t=>{const e=t;let a;return{hasLegacyControl:()=>{if(a===void 0){const l=e.label!==void 0||s(e),r=e.hasAttribute("aria-label")||e.hasAttribute("aria-labelledby")&&e.shadowRoot===null,n=o(e);a=e.legacy===!0||!l&&!r&&n!==null}return a}}},s=t=>!!(u.includes(t.tagName)&&t.querySelector('[slot="label"]')!==null||c.includes(t.tagName)&&t.textContent!==""),u=["ION-INPUT","ION-TEXTAREA","ION-SELECT","ION-RANGE"],c=["ION-TOGGLE","ION-CHECKBOX","ION-RADIO"];export{O as c};
---
+++ pr-preview/pr-367/vue/assets/index-CbWU7iKV.js
@@ -0,0 +1,1 @@
+import{M as a,a as M,u as n}from"./index-DGlVfcTW.js";import"./index-C9FUFRKN.js";import"./iframe-CcX-ZqVT.js";import"../sb-preview/runtime.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./doctrine-Z_8G-UDG.js";import"./index-DrFu-skq.js";export{a as MDXContext,M as MDXProvider,n as useMDXComponents};
