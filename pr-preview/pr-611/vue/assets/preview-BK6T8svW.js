b95ff307ae7e503f72e4e1f33fbc0f620272e7e0
felipefialho <felipefialho@users.noreply.github.com>
2024-11-06 20:19:16 +0000

    Remove preview for PR 611 🛬

--- pr-preview/pr-611/vue/assets/preview-BK6T8svW.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./DocsRenderer-NNNQARDV-BeZyS_0B.js","./chunk-HLWAVYOI-DL91RLLz.js","./iframe-CTx_Gebz.js","./index-CVKn_XfL.js","./_commonjsHelpers-Cpj98o6Y.js","./doctrine-Z_8G-UDG.js","./index-DrFu-skq.js"])))=>i.map(i=>d[i]);
-import{_ as e}from"./iframe-CTx_Gebz.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-BeZyS_0B.js"),__vite__mapDeps([0,1,2,3,4,5,6]),import.meta.url);return new r}}};export{a as parameters};
