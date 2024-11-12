bba2cec0a5904f855e053dfaba86aee3c505de2d
igorwessel <igorwessel@users.noreply.github.com>
2024-11-12 12:19:37 +0000

    Remove preview for PR 367 🛬

--- pr-preview/pr-367/vue/assets/DocsRenderer-NNNQARDV-NtSulAIQ.js
+++
@@ -1,1 +0,0 @@
-import{D as a,d}from"./chunk-HLWAVYOI-BJDRm-V4.js";import"./iframe-BBa450-8.js";import"../sb-preview/runtime.js";import"./index-DPDdKCib.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./doctrine-Z_8G-UDG.js";import"./index-DrFu-skq.js";export{a as DocsRenderer,d as defaultComponents};
--- pr-preview/pr-367/vue/assets/preview-Cim15kIN.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./DocsRenderer-NNNQARDV-NtSulAIQ.js","./chunk-HLWAVYOI-BJDRm-V4.js","./iframe-BBa450-8.js","./index-DPDdKCib.js","./_commonjsHelpers-Cpj98o6Y.js","./doctrine-Z_8G-UDG.js","./index-DrFu-skq.js"])))=>i.map(i=>d[i]);
-import{_ as e}from"./iframe-BBa450-8.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-NtSulAIQ.js"),__vite__mapDeps([0,1,2,3,4,5,6]),import.meta.url);return new r}}};export{a as parameters};
