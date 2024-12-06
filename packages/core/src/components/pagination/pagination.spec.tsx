ec94304d5fce24fa229b5e558fb5ddbb7f4c8229
Iogr Wessel <igor.silva@juntossomosmais.com.br>
2024-12-06 09:46:58 -0300

    test(pagination): add more case tests for pagination items

--- packages/core/src/components/pagination/pagination.spec.tsx
+++ packages/core/src/components/pagination/pagination.spec.tsx
@@ -52,64 +52,129 @@ describe('atom-pagination', () => {
       { type: 'page', number: 10 },
     ])
   })
-})
 
-it('disables previous button on first page', async () => {
-  const page = await newSpecPage({
-    components: [AtomPagination],
-    html: `<atom-pagination page="1" count="10"></atom-pagination>`,
+  it('generates correct boundary pagination items', async () => {
+    const page = await newSpecPage({
+      components: [AtomPagination],
+      html: `<atom-pagination page="6" count="50"></atom-pagination>`,
+    })
+
+    const pagination = page.rootInstance
+
+    pagination.boundary = 2
+    const items = pagination.makeItems()
+
+    // 1,2, ...rest-pages, 9, 10 <- should have two boundary pages in start and end
+    expect(items).toEqual([
+      { type: 'page', number: 1 },
+      { type: 'page', number: 2 },
+      { type: 'start-ellipsis' },
+      { type: 'page', number: 5 },
+      { type: 'page', number: 6 },
+      { type: 'page', number: 7 },
+      { type: 'end-ellipsis' },
+      { type: 'page', number: 49 },
+      { type: 'page', number: 50 },
+    ])
   })
 
-  const previousButton = page.root.shadowRoot.querySelector(
-    'atom-button[aria-label="Previous page"]'
-  )
+  it('generates correct start-ellipsis pagination when boundary is not reached', async () => {
+    const page = await newSpecPage({
+      components: [AtomPagination],
+      html: `<atom-pagination page="4" count="10"></atom-pagination>`,
+    })
 
-  expect(previousButton).toHaveAttribute('disabled')
-})
+    const pagination = page.rootInstance
+    const items = pagination.makeItems()
 
-it('disables next button on last page', async () => {
-  const page = await newSpecPage({
-    components: [AtomPagination],
-    html: `<atom-pagination page="10" count="10"></atom-pagination>`,
+    expect(items).toEqual([
+      { type: 'page', number: 1 },
+      { type: 'page', number: 2 },
+      { type: 'page', number: 3 },
+      { type: 'page', number: 4 },
+      { type: 'page', number: 5 },
+      { type: 'end-ellipsis' },
+      { type: 'page', number: 10 },
+    ])
   })
 
-  const nextButton = page.root.shadowRoot.querySelector(
-    'atom-button[aria-label="Next page"]'
-  )
+  it('generates correct end-ellipsis pagination when boundary is not reached', async () => {
+    const page = await newSpecPage({
+      components: [AtomPagination],
+      html: `<atom-pagination page="7" count="10"></atom-pagination>`,
+    })
 
-  expect(nextButton).toHaveAttribute('disabled')
-})
+    const pagination = page.rootInstance
+    const items = pagination.makeItems()
 
-it('enables next button when not on last page', async () => {
-  const page = await newSpecPage({
-    components: [AtomPagination],
-    html: `<atom-pagination page="5" count="10"></atom-pagination>`,
+    expect(items).toEqual([
+      { type: 'page', number: 1 },
+      { type: 'start-ellipsis' },
+      { type: 'page', number: 6 },
+      { type: 'page', number: 7 },
+      { type: 'page', number: 8 },
+      { type: 'page', number: 9 },
+      { type: 'page', number: 10 },
+    ])
   })
 
-  const nextButton = page.root.shadowRoot.querySelector(
-    'atom-button[aria-label="Next page"]'
-  )
+  it('disables previous button on last page', async () => {
+    const page = await newSpecPage({
+      components: [AtomPagination],
+      html: `<atom-pagination page="1" count="10"></atom-pagination>`,
+    })
 
-  expect(nextButton).not.toHaveAttribute('disabled')
-})
+    const previousButton = page.root.shadowRoot.querySelector(
+      'atom-button[aria-label="Previous page"]'
+    )
 
-it('emits atomChangePage event on next page click', async () => {
-  const page = await newSpecPage({
-    components: [AtomPagination],
-    html: `<atom-pagination page="1" count="10"></atom-pagination>`,
+    expect(previousButton).toHaveAttribute('disabled')
   })
 
-  const pagination = page.rootInstance
-  const nextButton = page.root.shadowRoot.querySelector(
-    'atom-button[aria-label="Next page"]'
-  )
+  it('disables next button on last page', async () => {
+    const page = await newSpecPage({
+      components: [AtomPagination],
+      html: `<atom-pagination page="10" count="10"></atom-pagination>`,
+    })
 
-  const changePageSpy = jest.spyOn(pagination.atomChangePage, 'emit')
+    const nextButton = page.root.shadowRoot.querySelector(
+      'atom-button[aria-label="Next page"]'
+    )
 
-  //@ts-expect-error is a test...
-  nextButton.click()
+    expect(nextButton).toHaveAttribute('disabled')
+  })
 
-  await page.waitForChanges()
+  it('enables next button when not on last page', async () => {
+    const page = await newSpecPage({
+      components: [AtomPagination],
+      html: `<atom-pagination page="5" count="10"></atom-pagination>`,
+    })
 
-  expect(changePageSpy).toHaveBeenCalledWith(2)
+    const nextButton = page.root.shadowRoot.querySelector(
+      'atom-button[aria-label="Next page"]'
+    )
+
+    expect(nextButton).not.toHaveAttribute('disabled')
+  })
+
+  it('emits atomChangePage event on next page click', async () => {
+    const page = await newSpecPage({
+      components: [AtomPagination],
+      html: `<atom-pagination page="1" count="10"></atom-pagination>`,
+    })
+
+    const pagination = page.rootInstance
+    const nextButton = page.root.shadowRoot.querySelector(
+      'atom-button[aria-label="Next page"]'
+    )
+
+    const changePageSpy = jest.spyOn(pagination.atomChangePage, 'emit')
+
+    //@ts-expect-error is a test...
+    nextButton.click()
+
+    await page.waitForChanges()
+
+    expect(changePageSpy).toHaveBeenCalledWith(2)
+  })
 })
