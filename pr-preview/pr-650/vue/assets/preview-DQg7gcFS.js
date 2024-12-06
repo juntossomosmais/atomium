28114a788f166cc15be8a1859d5f5a492e30ccdc
igorwessel <igorwessel@users.noreply.github.com>
2024-12-06 17:37:56 +0000

    Remove preview for PR 650 🛬

--- pr-preview/pr-650/vue/assets/preview-DQg7gcFS.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./DocsRenderer-NNNQARDV-DBiYRsqf.js","./chunk-HLWAVYOI-Bju0meXL.js","./iframe-CbalAcN4.js","./index-B6hqZ1Ev.js","./_commonjsHelpers-Cpj98o6Y.js","./doctrine-Z_8G-UDG.js","./index-DrFu-skq.js"])))=>i.map(i=>d[i]);
-import{_ as e}from"./iframe-CbalAcN4.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-DBiYRsqf.js"),__vite__mapDeps([0,1,2,3,4,5,6]),import.meta.url);return new r}}};export{a as parameters};
