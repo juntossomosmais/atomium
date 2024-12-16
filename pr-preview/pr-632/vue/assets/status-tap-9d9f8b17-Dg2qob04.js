b70631a698c304e23b528559323af5ab0d9a513e
felipefialho <felipefialho@users.noreply.github.com>
2024-12-16 20:48:54 +0000

    Deploy preview for PR 632 🛫

--- pr-preview/pr-632/vue/assets/status-tap-9d9f8b17-Dg2qob04.js
+++
@@ -1,3 +0,0 @@
-import{d as i,w as a}from"./index-BhD6i04_.js";import{a as m,s as p}from"./index-74a0cdd4-i6ycB-rg.js";import{e as d}from"./helpers-b14eeb70-DAoIUSfU.js";import"./vue.esm-bundler-HaV7LZ5z.js";import"./iframe-BQIjXwlP.js";import"../sb-preview/runtime.js";import"./index-f5dbb989-By3l89LO.js";/*!
- * (C) Ionic http://ionicframework.com - MIT License
- */const u=()=>{const o=window;o.addEventListener("statusTap",()=>{i(()=>{const n=o.innerWidth,r=o.innerHeight,e=document.elementFromPoint(n/2,r/2);if(!e)return;const t=m(e);t&&new Promise(s=>d(t,s)).then(()=>{a(async()=>{t.style.setProperty("--overflow","hidden"),await p(t,300),t.style.removeProperty("--overflow")})})})})};export{u as startStatusTap};
