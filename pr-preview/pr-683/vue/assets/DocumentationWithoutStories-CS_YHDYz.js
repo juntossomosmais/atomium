36ffe63b5feae6fcbf2f2ec084cff0fdf16f9554
dyhalmeida <dyhalmeida@users.noreply.github.com>
2025-01-03 17:45:51 +0000

    Remove preview for PR 683 🛬

--- pr-preview/pr-683/vue/assets/DocumentationWithoutStories-CS_YHDYz.js
+++
@@ -1,6 +0,0 @@
-import{ae as n,af as a,ag as r,ah as i,ai as x,aj as c}from"./index-CCkzfmfR.js";import{u as o}from"./index-on5TyXXX.js";function e(s){const t={h3:"h3",...o(),...s.components};return n.jsxs(n.Fragment,{children:[n.jsx(a,{isTemplate:!0}),`
-`,n.jsx(r,{}),`
-`,n.jsx(i,{}),`
-`,n.jsx(x,{}),`
-`,n.jsx(t.h3,{id:"props",children:"Props"}),`
-`,n.jsx(c,{})]})}function p(s={}){const{wrapper:t}={...o(),...s.components};return t?n.jsx(t,{...s,children:n.jsx(e,{...s})}):e(s)}export{p as M};
