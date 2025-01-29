01860bcfedee9ddbe52e7f05ebe67531846e49b8
felipefialho <felipefialho@users.noreply.github.com>
2025-01-29 13:35:06 +0000

    Deploy preview for PR 687 🛫

--- pr-preview/pr-687/vue/assets/menu-toggle-util-89e75078-BMAG9KhA.js
+++
@@ -1,3 +0,0 @@
-import{m as a}from"./index-93f47ab1-D1jE2MGQ.js";/*!
- * (C) Ionic http://ionicframework.com - MIT License
- */const n=async i=>{const t=await a.get(i);return!!(t&&await t.isActive())};export{n as u};
