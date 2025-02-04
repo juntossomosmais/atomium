d65f0bfc3e52bff20d3ecd7e5f596ad6794e9faf
felipefialho <felipefialho@users.noreply.github.com>
2025-02-04 22:11:31 +0000

    Remove preview for PR 697 🛬

--- pr-preview/pr-697/vue/assets/DocumentationWithoutStories-XZblD3gk.js
+++
@@ -1,6 +0,0 @@
-import{j as n,M as r,T as a,D as i,P as x,C as c}from"./index-Bk5T3EL6.js";import{useMDXComponents as o}from"./index-CMwEwakC.js";function e(t){const s={h3:"h3",...o(),...t.components};return n.jsxs(n.Fragment,{children:[n.jsx(r,{isTemplate:!0}),`
-`,n.jsx(a,{}),`
-`,n.jsx(i,{}),`
-`,n.jsx(x,{}),`
-`,n.jsx(s.h3,{id:"props",children:"Props"}),`
-`,n.jsx(c,{})]})}function p(t={}){const{wrapper:s}={...o(),...t.components};return s?n.jsx(s,{...t,children:n.jsx(e,{...t})}):e(t)}export{p as M};
