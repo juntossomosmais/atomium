7cce35c2d53a48bda9bbc18254263aa4270ab53e
gabrielduete <gabrielduete@users.noreply.github.com>
2025-02-04 23:07:29 +0000

    Deploy preview for PR 695 🛫

---
+++ pr-preview/pr-695/vue/assets/DocumentationWithoutStories-CL6AHGoo.js
@@ -0,0 +1,6 @@
+import{j as n,M as r,T as a,D as i,P as x,C as c}from"./index-BXa96SPY.js";import{useMDXComponents as o}from"./index-BCzwIFOw.js";function e(t){const s={h3:"h3",...o(),...t.components};return n.jsxs(n.Fragment,{children:[n.jsx(r,{isTemplate:!0}),`
+`,n.jsx(a,{}),`
+`,n.jsx(i,{}),`
+`,n.jsx(x,{}),`
+`,n.jsx(s.h3,{id:"props",children:"Props"}),`
+`,n.jsx(c,{})]})}function p(t={}){const{wrapper:s}={...o(),...t.components};return s?n.jsx(s,{...t,children:n.jsx(e,{...t})}):e(t)}export{p as M};
