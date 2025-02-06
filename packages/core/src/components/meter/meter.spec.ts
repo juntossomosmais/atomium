1465f8ac06ba2feaf3a56cdcda7ec636b3f493c7
Gabriel Duete <gabrielmonteiroduete@gmail.com>
2025-02-06 15:09:49 -0300

    test(meter): add test mobile case

--- packages/core/src/components/meter/meter.spec.ts
+++ packages/core/src/components/meter/meter.spec.ts
@@ -1,5 +1,7 @@
 import { newSpecPage } from '@stencil/core/testing'
 
+import * as screens from '../../utils/screens'
+
 import { AtomMeter } from './meter'
 
 describe('atom-meter', () => {
@@ -97,6 +99,34 @@ describe('atom-meter', () => {
           </div>
           <div class="atom-meter is-large">
             <div class="within is-neutral" style="width: 60%;"></div>
+          </div>
+        </mock:shadow-root>
+        Children
+      </atom-meter>
+    `)
+  })
+
+  it('should render mobile children', async () => {
+    jest.spyOn(screens, 'isMobile').mockReturnValue(true)
+
+    const page = await newSpecPage({
+      components: [AtomMeter],
+      html: `<atom-meter title="test" min="1" max="6" actual="4">Children</atom-meter>`,
+    })
+
+    await page.waitForChanges()
+
+    expect(page.root).toEqualHtml(`
+      <atom-meter title="test" min="1" max="6" actual="4">
+        <mock:shadow-root>
+          <div class="container-text">
+            <h1 class="title">test</h1>
+          </div>
+          <div class="atom-meter is-large">
+            <div class="within is-neutral" style="width: 60%;"></div>
+          </div>
+          <div class="container-text has-gap-top">
+            <slot></slot>
           </div>
         </mock:shadow-root>
         Children
