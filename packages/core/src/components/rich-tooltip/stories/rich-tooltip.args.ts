204767a10e9ec17c9a258be278896a264cc2a316
Gabriel Duete <gabrielmonteiroduete@gmail.com>
2025-02-06 15:50:41 -0300

    feat(rich-tooltip): add initial files stories

---
+++ packages/core/src/components/rich-tooltip/stories/rich-tooltip.args.ts
@@ -0,0 +1,36 @@
+export const BadgeStoryArgs = {
+  decorators: [],
+  parameters: {
+    actions: {
+      handles: [],
+    },
+    docs: {
+      description: {
+        component:
+          'Badges are inline block elements that usually appear near another element. Read the [Ionic documentation](https://ionicframework.com/docs/api/Badge) for more information about this component.',
+      },
+    },
+  },
+  argTypes: {
+    type: {
+      control: 'select',
+      options: [
+        'primary',
+        'secondary',
+        'info',
+        'success',
+        'warning',
+        'danger',
+        'neutral',
+        'dark',
+      ],
+      defaultValue: { summary: 'primary' },
+      description:
+        'The type of the badge, the component will receive the color according to its type.',
+    },
+    label: {
+      control: 'text',
+      description: 'The label of the badge',
+    },
+  },
+}
