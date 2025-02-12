5b0c46a273c33ade9657d9ae07b54562d55413ff
brunosf <brunosf@users.noreply.github.com>
2025-02-12 17:33:57 +0000

    Deploy preview for PR 700 🛫

---
+++ pr-preview/pr-700/vue/assets/menu-toggle-util-d7b5c45d-BW8uLtmP.js
@@ -0,0 +1,3 @@
+import{m as a}from"./index-1fe4c732-CaSbLNhA.js";/*!
+ * (C) Ionic http://ionicframework.com - MIT License
+ */const n=async i=>{const t=await a.get(i);return!!(t&&await t.isActive())};export{n as u};
