335cf05bd8a5b894feaf5b698bb2724d5caf07a5
gabrielduete <gabrielduete@users.noreply.github.com>
2025-02-14 14:52:11 +0000

    Deploy preview for PR 698 🛫

---
+++ pr-preview/pr-698/vue/assets/status-tap-082048fe-padbNwhY.js
@@ -0,0 +1,3 @@
+import{j as i,w as a}from"./index-DZXQTcpR.js";import{a as m,s as p}from"./index-c0cf4f1b-DuByj2iv.js";import{e as c}from"./helpers-4047ce7f-ZS1Xs9E2.js";import"./iframe-8LX55Q3q.js";import"./index-1e38abc5-CfLTl0tl.js";/*!
+ * (C) Ionic http://ionicframework.com - MIT License
+ */const y=()=>{const o=window;o.addEventListener("statusTap",()=>{i(()=>{const n=o.innerWidth,r=o.innerHeight,e=document.elementFromPoint(n/2,r/2);if(!e)return;const t=m(e);t&&new Promise(s=>c(t,s)).then(()=>{a(async()=>{t.style.setProperty("--overflow","hidden"),await p(t,300),t.style.removeProperty("--overflow")})})})})};export{y as startStatusTap};
