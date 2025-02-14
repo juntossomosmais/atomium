31101287d08cd9e57234f72123ed5ba7c2210b4e
Gabriel Duete <gabrielmonteiroduete@gmail.com>
2025-02-14 16:43:02 -0300

    feat(docs): add table component for displaying data in a tabular format

---
+++ packages/tokens/stories/components/Table.jsx
@@ -0,0 +1,25 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+const Table = ({ headers, rows }) => (
+  <table>
+    <thead>
+      <tr>
+        {headers.map((header, index) => (
+          <th key={index}>{header}</th>
+        ))}
+      </tr>
+    </thead>
+    <tbody>
+      {rows.map((row, rowIndex) => (
+        <tr key={rowIndex}>
+          {row.map((cell, cellIndex) => (
+            <td key={cellIndex}>{cell}</td>
+          ))}
+        </tr>
+      ))}
+    </tbody>
+  </table>
+)
+
+export default Table
