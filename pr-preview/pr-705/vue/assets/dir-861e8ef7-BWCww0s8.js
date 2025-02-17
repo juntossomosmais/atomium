078dbb10c5d096fef1b24acf9511acab66481ea3
gabrielduete <gabrielduete@users.noreply.github.com>
2025-02-17 13:14:36 +0000

    Remove preview for PR 705 🛬

--- pr-preview/pr-705/vue/assets/dir-861e8ef7-BWCww0s8.js
+++
@@ -1,3 +0,0 @@
-/*!
- * (C) Ionic http://ionicframework.com - MIT License
- */const r=e=>e&&e.dir!==""?e.dir.toLowerCase()==="rtl":(document==null?void 0:document.dir.toLowerCase())==="rtl";export{r as i};
