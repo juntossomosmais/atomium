28666d9f3b87b2d3729fe0f8983725705fd00dd1
gabrielduete <gabrielmonteiroduete@gmail.com>
2025-02-17 16:08:27 +0000

    Deploying to gh-pages from @ juntossomosmais/atomium@192b380658685bc5f114ae51e2bb592434819de6 🚀

--- vue/assets/DocumentationWithoutStories-DP_F9DBT.js
+++
@@ -1,6 +0,0 @@
-import{j as n,M as r,T as a,D as i,P as x,C as c}from"./index-CRJDNYUz.js";import{useMDXComponents as o}from"./index-N-V2xOah.js";function e(t){const s={h3:"h3",...o(),...t.components};return n.jsxs(n.Fragment,{children:[n.jsx(r,{isTemplate:!0}),`
-`,n.jsx(a,{}),`
-`,n.jsx(i,{}),`
-`,n.jsx(x,{}),`
-`,n.jsx(s.h3,{id:"props",children:"Props"}),`
-`,n.jsx(c,{})]})}function p(t={}){const{wrapper:s}={...o(),...t.components};return s?n.jsx(s,{...t,children:n.jsx(e,{...t})}):e(t)}export{p as M};
