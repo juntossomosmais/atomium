29f947517b0f8a7058bf4247d29ba88849269be7
gabrielduete <gabrielmonteiroduete@gmail.com>
2025-02-20 17:23:56 +0000

    Deploying to gh-pages from @ juntossomosmais/atomium@1460bd885a1310cd4744cfbf35a3f3a2a1335944 🚀

--- vue/assets/status-tap-082048fe-rjcS_pIy.js
+++
@@ -1,3 +0,0 @@
-import{j as i,w as a}from"./index-CsJWRV96.js";import{a as m,s as p}from"./index-c0cf4f1b-DuByj2iv.js";import{e as c}from"./helpers-4047ce7f-ZS1Xs9E2.js";import"./iframe-CQ63PS9t.js";import"./index-1e38abc5-CfLTl0tl.js";/*!
- * (C) Ionic http://ionicframework.com - MIT License
- */const y=()=>{const o=window;o.addEventListener("statusTap",()=>{i(()=>{const n=o.innerWidth,r=o.innerHeight,e=document.elementFromPoint(n/2,r/2);if(!e)return;const t=m(e);t&&new Promise(s=>c(t,s)).then(()=>{a(async()=>{t.style.setProperty("--overflow","hidden"),await p(t,300),t.style.removeProperty("--overflow")})})})})};export{y as startStatusTap};
