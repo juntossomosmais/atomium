39176d44f66ad0af386281951ae0d6cbe2b4b70e
Gabriel Duete <gabrielmonteiroduete@gmail.com>
2025-03-20 18:32:00 -0300

    feat: configure styles and props

--- packages/core/src/components/stepper/stepper.tsx
+++ packages/core/src/components/stepper/stepper.tsx
@@ -1,4 +1,4 @@
-import { Component, Host, h } from '@stencil/core'
+import { Component, Host, Prop, h } from '@stencil/core'
 
 @Component({
   tag: 'atom-stepper',
@@ -6,10 +6,52 @@ import { Component, Host, h } from '@stencil/core'
   shadow: true,
 })
 export class AtomStepper {
+  @Prop() steps: {
+    title: string
+    completed: boolean
+    actual?: boolean
+    disabled?: boolean
+  }[]
+
+  private readonly isCompleted = (completed: boolean) =>
+    completed ? 'completed' : ''
+
+  private readonly isLastActive = (index: number) => {
+    const currentStep = this.steps[index]
+    const nextStep = this.steps[index + 1]
+
+    const currentStepIsTheLastCompleted =
+      currentStep.completed && nextStep && !nextStep.completed
+
+    return currentStepIsTheLastCompleted ? 'last' : ''
+  }
+
   render() {
     return (
       <Host>
-        <div></div>
+        <ul class='atom-stepper'>
+          {this.steps.map(({ title, completed, actual, disabled }, index) => (
+            <li
+              class={`
+                step
+                ${actual ? 'active' : ''}
+                ${this.isLastActive(index)} ${this.isCompleted(completed)}
+                ${this.isLastActive(index)}
+                ${disabled ? 'disabled' : ''}
+              `}
+              key={index}
+            >
+              <div class='number'>
+                {this.isCompleted(completed) ? (
+                  <atom-icon icon='check' color='white' size={16} />
+                ) : (
+                  index + 1
+                )}
+              </div>
+              <span class='title'>{title}</span>
+            </li>
+          ))}
+        </ul>
       </Host>
     )
   }
