ba847dad138fefcbe423eb676f4dba718d8683de
gabrielduete <gabrielduete@users.noreply.github.com>
2025-03-27 21:07:51 +0000

    Deploy preview for PR 730 🛫

---
+++ pr-preview/pr-730/vue/assets/menu-toggle-util-7eb4bcbc-BQQQuuA7.js
@@ -0,0 +1,3 @@
+import{m as a}from"./index-5b47386a-CL_PqkD8.js";/*!
+ * (C) Ionic http://ionicframework.com - MIT License
+ */const n=async i=>{const t=await a.get(i);return!!(t&&await t.isActive())};export{n as u};
