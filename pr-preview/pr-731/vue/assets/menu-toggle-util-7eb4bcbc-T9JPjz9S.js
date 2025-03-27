cc5610391440d774dc0cea9c1b562bf1f3eb432f
tassioFront <tassioFront@users.noreply.github.com>
2025-03-27 21:19:04 +0000

    Deploy preview for PR 731 🛫

---
+++ pr-preview/pr-731/vue/assets/menu-toggle-util-7eb4bcbc-T9JPjz9S.js
@@ -0,0 +1,3 @@
+import{m as a}from"./index-5b47386a-BVOhaPgB.js";/*!
+ * (C) Ionic http://ionicframework.com - MIT License
+ */const n=async i=>{const t=await a.get(i);return!!(t&&await t.isActive())};export{n as u};
