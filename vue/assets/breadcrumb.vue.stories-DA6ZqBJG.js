14ebacbbe9e5d7e3aa2d57eee2afea78e0d3803b
tassioFront <tassio72@hotmail.com>
2025-03-28 14:19:20 +0000

    Deploying to gh-pages from @ juntossomosmais/atomium@0e91bc55185d1e529fc7cbca20fb1b79e499c960 🚀

---
+++ vue/assets/breadcrumb.vue.stories-DA6ZqBJG.js
@@ -0,0 +1,17 @@
+import{k as s}from"./index-DivYdEX9.js";import{C as n}from"./table-D060eKuu.js";import"./vue.esm-bundler-UQm0beuB.js";import"./iframe-3VZcyeil.js";const a={decorators:[],parameters:{actions:{handles:[]},docs:{description:{component:"Breadcrumbs are navigation items that are used to indicate where a user is on site. Read the [Ionic documentation](https://ionicframework.com/docs/api/breadcrumbs) for more information about this component."}}},argTypes:{items:{description:"This is the list of items that will be displayed in the breadcrumb. Items must have `title` and `text` and optionally a `redirect` function when clicking on the item.",table:{category:n.PROPERTIES}}}},p={title:"Components/Breadcrumb",...a},l=t=>({components:{AtomBreadcrumb:s},setup(){return{args:t}},template:'<AtomBreadcrumb :items="args.items" />'}),e={render:t=>l(t),args:{items:[{text:"First level",title:"Go to first level",redirect:()=>console.log("/first")},{text:"Intermediary level",title:"Go to intermediary level",redirect:()=>console.log("/intermediary")},{text:"Current level",title:"This is the current level"}]}};var r,o,i;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  render: args => createBreadcrumb(args),
+  args: {
+    items: [{
+      text: 'First level',
+      title: 'Go to first level',
+      redirect: () => console.log('/first')
+    }, {
+      text: 'Intermediary level',
+      title: 'Go to intermediary level',
+      redirect: () => console.log('/intermediary')
+    }, {
+      text: 'Current level',
+      title: 'This is the current level'
+    }]
+  }
+}`,...(i=(o=e.parameters)==null?void 0:o.docs)==null?void 0:i.source}}};const h=["Default"];export{e as Default,h as __namedExportsOrder,p as default};
