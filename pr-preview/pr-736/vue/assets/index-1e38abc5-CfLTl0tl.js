02bde79978af0f16f8d59207ccb11c33eb640ba4
felipefialho <felipefialho@users.noreply.github.com>
2025-04-03 21:11:34 +0000

    Remove preview for PR 736 🛬

--- pr-preview/pr-736/vue/assets/index-1e38abc5-CfLTl0tl.js
+++
@@ -1,3 +0,0 @@
-/*!
- * (C) Ionic http://ionicframework.com - MIT License
- */const o=(r,...n)=>console.warn(`[Ionic Warning]: ${r}`,...n),e=(r,...n)=>console.error(`[Ionic Error]: ${r}`,...n),t=(r,...n)=>console.error(`<${r.tagName.toLowerCase()}> must be used inside ${n.join(" or ")}.`);export{t as a,e as b,o as p};
