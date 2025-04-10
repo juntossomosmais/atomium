ce237dd42a74966b4826bfb85d76ff2bbca1c8dd
tassioFront <tassio72@hotmail.com>
2025-04-10 21:01:54 +0000

    Deploying to gh-pages from @ juntossomosmais/atomium@dc4a00f730204b86bf4374910eacaf34dbb72548 🚀

--- vue/assets/status-tap-a1c4ee2c-D3hUpv6-.js
+++
@@ -1,3 +0,0 @@
-import{d as i,w as a}from"./index-BY_xprlD.js";import{a as m,s as p}from"./index-c0cf4f1b-DQm7UrJT.js";import{e as d}from"./helpers-4047ce7f-B-sfu2GX.js";import"./vue.esm-bundler-UQm0beuB.js";import"./iframe-MNxWBaSk.js";import"./index-1e38abc5-CfLTl0tl.js";/*!
- * (C) Ionic http://ionicframework.com - MIT License
- */const T=()=>{const o=window;o.addEventListener("statusTap",()=>{i(()=>{const n=o.innerWidth,r=o.innerHeight,e=document.elementFromPoint(n/2,r/2);if(!e)return;const t=m(e);t&&new Promise(s=>d(t,s)).then(()=>{a(async()=>{t.style.setProperty("--overflow","hidden"),await p(t,300),t.style.removeProperty("--overflow")})})})})};export{T as startStatusTap};
