7dad9d1d189117e4c52ef07301b35f9957dd778e
Gabriel Duete <gabrielmonteiroduete@gmail.com>
2025-05-02 19:41:02 -0300

    feat: adjust stories react and vue

--- packages/core/src/components/stepper/stories/stepper.vue.stories.tsx
+++ packages/core/src/components/stepper/stories/stepper.vue.stories.tsx
@@ -14,19 +14,59 @@ const createStepper = (args) => ({
     return { args }
   },
   template: `
-    <AtomStepper steps="${args.steps}"></AtomStepper>
+    <AtomStepper
+      :steps="args.steps"
+      :activeStep="args.activeStep"
+      :disabledUnrenderedStep="args.disabledUnrenderedStep"
+      :stepTitle="args.stepTitle">
+    </AtomStepper>
   `,
 })
 
-export const Primary: StoryObj = {
+export const InProgress: StoryObj = {
   render: (args) => createStepper(args),
   args: {
     steps: [
       { title: 'Step 1', completed: true },
       { title: 'Step 2', completed: true },
-      { title: 'Step 3', completed: false, actual: true },
-      { title: 'Step 4', completed: false, disabled: true },
-      { title: 'Step 5', completed: false, disabled: true },
+      { title: 'Step 3', completed: false },
+      { title: 'Step 4', completed: false },
+      { title: 'Step 5', completed: false },
     ],
+    activeStep: 2,
+    disabledUnrenderedStep: true,
+    stepTitle: 'Etapa 1 de 5',
+  },
+}
+
+export const Uncompleted: StoryObj = {
+  render: (args) => createStepper(args),
+  args: {
+    steps: [
+      { title: 'Step 1', completed: false },
+      { title: 'Step 2', completed: false },
+      { title: 'Step 3', completed: false },
+      { title: 'Step 4', completed: false },
+      { title: 'Step 5', completed: false },
+    ],
+    activeStep: 0,
+    disabledUnrenderedStep: true,
+    stepTitle: 'Progress 1 of 5',
+  },
+}
+
+export const Completed: StoryObj = {
+  render: (args) => createStepper(args),
+  args: {
+    steps: [
+      { title: 'Step 1', completed: true },
+      { title: 'Step 2', completed: true },
+      { title: 'Step 3', completed: true },
+      { title: 'Step 4', completed: true },
+      { title: 'Step 5', completed: true },
+    ],
+    activeStep: 4,
+    disabledUnrenderedStep: false,
+    stepTitle: 'Etapa 1 de 5',
   },
 }
