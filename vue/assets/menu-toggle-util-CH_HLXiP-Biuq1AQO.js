76b61789740b3b9c627e4217aa8a63ca0a45d858
felipefialho <hi@felipefialho.com>
2025-05-12 14:03:52 +0000

    Deploying to gh-pages from @ juntossomosmais/atomium@8954e4d4206fb4bcc3959d10ca1ba6d977105991 🚀

---
+++ vue/assets/menu-toggle-util-CH_HLXiP-Biuq1AQO.js
@@ -0,0 +1,3 @@
+import{m as a}from"./index-BJYlUqQq-D-tNM4gN.js";/*!
+ * (C) Ionic http://ionicframework.com - MIT License
+ */const n=async i=>{const t=await a.get(i);return!!(t&&await t.isActive())};export{n as u};
