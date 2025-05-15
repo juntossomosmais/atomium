da9bfe83eea84e5e35277ef3a5dedc823d3c3e71
Gabriel Duete <gabrielmonteiroduete@gmail.com>
2025-05-15 11:37:15 -0300

    feat: remove unncessary parsed steps to pass tests

--- packages/core/src/components/stepper/stepper.spec.ts
+++ packages/core/src/components/stepper/stepper.spec.ts
@@ -5,23 +5,32 @@ import * as screens from '../../utils/screens'
 import { AtomStepper } from './stepper'
 
 describe('atom-stepper', () => {
-  it('should render with default props', async () => {
+  it('should render completed steps', async () => {
     const page = await newSpecPage({
       components: [AtomStepper],
-      html: `<atom-stepper steps='[{"title":"Step 1","completed":false},{"title":"Step 2","completed":false}]' active-step="0" role="stepper"></atom-stepper>`,
+      html: `<atom-stepper active-step="1" role="stepper"></atom-stepper>`,
     })
 
+    page.rootInstance.steps = [
+      { title: 'Step 1', completed: true },
+      { title: 'Step 2', completed: false },
+    ]
+
+    await page.waitForChanges()
+
     expect(page.root).toEqualHtml(`
-      <atom-stepper active-step="0" role="stepper" steps='[{"title":"Step 1","completed":false},{"title":"Step 2","completed":false}]'>
+      <atom-stepper active-step="1" role="stepper">
         <mock:shadow-root>
           <ul class="atom-stepper">
-            <li class="step">
-              <div class="number">1</div>
-              <span class="is-active title">Step 1</span>
+            <li class="is-completed is-last step">
+              <div class="number">
+                <atom-icon icon="check" color="white" size="16"></atom-icon>
+              </div>
+              <span class="title">Step 1</span>
             </li>
-            <li class="step is-disabled">
+            <li class="step">
               <div class="number">2</div>
-              <span class="title">Step 2</span>
+              <span class="is-active title">Step 2</span>
             </li>
           </ul>
         </mock:shadow-root>
@@ -32,11 +41,18 @@ describe('atom-stepper', () => {
   it('should render completed steps', async () => {
     const page = await newSpecPage({
       components: [AtomStepper],
-      html: `<atom-stepper steps='[{"title":"Step 1","completed":true},{"title":"Step 2","completed":false}]' active-step="1" role="stepper"></atom-stepper>`,
+      html: `<atom-stepper active-step="1" role="stepper"></atom-stepper>`,
     })
 
+    page.rootInstance.steps = [
+      { title: 'Step 1', completed: true },
+      { title: 'Step 2', completed: false },
+    ]
+
+    await page.waitForChanges()
+
     expect(page.root).toEqualHtml(`
-      <atom-stepper active-step="1" role="stepper" steps='[{"title":"Step 1","completed":true},{"title":"Step 2","completed":false}]'>
+      <atom-stepper active-step="1" role="stepper">
         <mock:shadow-root>
           <ul class="atom-stepper">
             <li class="is-completed is-last step">
@@ -58,11 +74,18 @@ describe('atom-stepper', () => {
   it('should disable unrendered steps', async () => {
     const page = await newSpecPage({
       components: [AtomStepper],
-      html: `<atom-stepper steps='[{"title":"Step 1","completed":false},{"title":"Step 2","completed":false}]' active-step="0" role="stepper" disabled-unrendered-step="true"></atom-stepper>`,
+      html: `<atom-stepper  active-step="0" role="stepper" disabled-unrendered-step="true"></atom-stepper>`,
     })
 
+    page.rootInstance.steps = [
+      { title: 'Step 1', completed: false },
+      { title: 'Step 2', completed: false },
+    ]
+
+    await page.waitForChanges()
+
     expect(page.root).toEqualHtml(`
-      <atom-stepper active-step="0" role="stepper" steps='[{"title":"Step 1","completed":false},{"title":"Step 2","completed":false}]' disabled-unrendered-step="true">
+      <atom-stepper active-step="0" role="stepper" disabled-unrendered-step="true">
         <mock:shadow-root>
           <ul class="atom-stepper">
             <li class="step">
@@ -85,11 +108,15 @@ it('should render mobile view', async () => {
 
   const page = await newSpecPage({
     components: [AtomStepper],
-    html: `<atom-stepper steps='[{"title":"Step 1","completed":false}]' active-step="0" role="stepper" step-title="Mobile Step"></atom-stepper>`,
+    html: `<atom-stepper active-step="0" role="stepper" step-title="Mobile Step"></atom-stepper>`,
   })
 
+  page.rootInstance.steps = [{ title: 'Step 1', completed: false }]
+
+  await page.waitForChanges()
+
   expect(page.root).toEqualHtml(`
-    <atom-stepper active-step="0" role="stepper" steps='[{"title":"Step 1","completed":false}]' step-title="Mobile Step">
+    <atom-stepper active-step="0" role="stepper" step-title="Mobile Step">
       <mock:shadow-root>
         <div>
           <div class="atom-stepper-mobile">
--- packages/core/src/components/stepper/stepper.tsx
+++ packages/core/src/components/stepper/stepper.tsx
@@ -1,4 +1,4 @@
-import { Component, Host, Prop, Watch, h } from '@stencil/core'
+import { Component, Host, Prop, h } from '@stencil/core'
 import DOMPurify from 'dompurify'
 
 import { isMaxTablet } from '../../utils/screens'
@@ -9,40 +9,20 @@ import { isMaxTablet } from '../../utils/screens'
   shadow: true,
 })
 export class AtomStepper {
-  @Prop({ mutable: true }) steps:
-    | string
-    | {
-        title: string
-        completed: boolean
-      }[] = []
+  @Prop({ mutable: true }) steps: Array<{
+    title: string
+    completed: boolean
+  }> = []
   @Prop() activeStep: number
   @Prop() stepTitle?: string
   @Prop() disabledUnrenderedStep?: boolean = true
-
-  private parsedSteps: {
-    title: string
-    completed: boolean
-  }[] = []
-
-  @Watch('steps')
-  parseSteps(newVal) {
-    this.parsedSteps = typeof newVal === 'string' ? JSON.parse(newVal) : newVal
-  }
-
-  componentWillLoad() {
-    if (typeof this.steps === 'string') {
-      this.parsedSteps = JSON.parse(this.steps)
-    } else {
-      this.parsedSteps = this.steps
-    }
-  }
 
   private readonly addCompletedClass = (completed: boolean) =>
     completed ? 'is-completed' : ''
 
   private readonly isLastActive = (index: number) => {
-    const currentStep = this.parsedSteps[index]
-    const nextStep = this.parsedSteps[index + 1]
+    const currentStep = this.steps[index]
+    const nextStep = this.steps[index + 1]
 
     const currentStepIsTheLastCompleted =
       currentStep?.completed && nextStep && !nextStep.completed
@@ -51,7 +31,7 @@ export class AtomStepper {
   }
 
   render() {
-    const actualStep = this.parsedSteps[this.activeStep]
+    const actualStep = this.steps[this.activeStep]
     const completedStep = actualStep?.completed
 
     return (
@@ -81,7 +61,7 @@ export class AtomStepper {
           </div>
         ) : (
           <ul class='atom-stepper'>
-            {this.parsedSteps.map(({ title, completed }, index) => {
+            {this.steps.map(({ title, completed }, index) => {
               const isActive = index === this.activeStep
               const shouldDisabled =
                 this.disabledUnrenderedStep && index > this.activeStep
