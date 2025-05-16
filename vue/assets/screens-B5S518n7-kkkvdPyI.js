1db4e064ad463793c506bb839aeee4bce00d7334
gabrielduete <gabrielmonteiroduete@gmail.com>
2025-05-16 19:02:11 +0000

    Deploying to gh-pages from @ juntossomosmais/atomium@960354046f34cfe9a44c772a0bb8483cb7726db7 🚀

---
+++ vue/assets/screens-B5S518n7-kkkvdPyI.js
@@ -0,0 +1,1 @@
+const a=()=>window.matchMedia("(max-width: 768px)").matches,t=()=>window.matchMedia("(max-width: 991px)").matches;export{t as a,a as i};
--- vue/assets/status-tap-Dzyd6JEA-W4kT_btr.js
+++
@@ -1,3 +0,0 @@
-import{e as i,w as a}from"./index-tzTr3s0c.js";import{f as m,s as c}from"./index-Ck53J8br-D6yij4ST.js";import{c as p}from"./helpers-DFySOcQU-CIfPvRn-.js";import"./vue.esm-bundler-UQm0beuB.js";import"./iframe-DwfdPbka.js";/*!
- * (C) Ionic http://ionicframework.com - MIT License
- */const y=()=>{const o=window;o.addEventListener("statusTap",()=>{i(()=>{const n=o.innerWidth,r=o.innerHeight,e=document.elementFromPoint(n/2,r/2);if(!e)return;const t=m(e);t&&new Promise(s=>p(t,s)).then(()=>{a(async()=>{t.style.setProperty("--overflow","hidden"),await c(t,300),t.style.removeProperty("--overflow")})})})})};export{y as startStatusTap};
