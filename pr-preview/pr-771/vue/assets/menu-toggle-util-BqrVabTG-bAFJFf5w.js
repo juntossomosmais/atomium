57105510cb16a1744bc05772f5e24ecbb3fa2985
felipefialho <felipefialho@users.noreply.github.com>
2025-07-07 14:50:17 +0000

    Deploy preview for PR 771 🛫

---
+++ pr-preview/pr-771/vue/assets/menu-toggle-util-BqrVabTG-bAFJFf5w.js
@@ -0,0 +1,3 @@
+import{m as a}from"./index-hZk6uXNL-CIvkv33O.js";/*!
+ * (C) Ionic http://ionicframework.com - MIT License
+ */const n=async i=>{const t=await a.get(i);return!!(t&&await t.isActive())};export{n as u};
