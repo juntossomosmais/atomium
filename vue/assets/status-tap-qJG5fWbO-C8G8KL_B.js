1947ab69cd5ee0a98aac31c30dd52064578fbe95
felipefialho <hi@felipefialho.com>
2025-07-17 19:54:17 +0000

    Deploying to gh-pages from @ juntossomosmais/atomium@442d3554373e7e3adec95d3e52abcef9e8a01711 🚀

---
+++ vue/assets/status-tap-qJG5fWbO-C8G8KL_B.js
@@ -0,0 +1,3 @@
+import{e as i,w as a}from"./index-BG4G2zIL.js";import{f as m,s as c}from"./index-q5Up1lxZ-Cjjr1R5W.js";import{c as p}from"./helpers-dt_t6yMe-C1VykClD.js";import"./vue.esm-bundler-UQm0beuB.js";import"./iframe-BWnbrByL.js";/*!
+ * (C) Ionic http://ionicframework.com - MIT License
+ */const y=()=>{const o=window;o.addEventListener("statusTap",()=>{i(()=>{const n=o.innerWidth,r=o.innerHeight,e=document.elementFromPoint(n/2,r/2);if(!e)return;const t=m(e);t&&new Promise(s=>p(t,s)).then(()=>{a(async()=>{t.style.setProperty("--overflow","hidden"),await c(t,300),t.style.removeProperty("--overflow")})})})})};export{y as startStatusTap};
