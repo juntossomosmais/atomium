8b8b33826a86993398f896e5fb70792f29e5feeb
tassioFront <tassioFront@users.noreply.github.com>
2025-08-06 19:08:00 +0000

    Deploy preview for PR 780 🛫

---
+++ pr-preview/pr-780/vue/assets/status-tap-qJG5fWbO-BBkjHuK5.js
@@ -0,0 +1,3 @@
+import{e as i,w as a}from"./index-owxOSzyM.js";import{f as m,s as c}from"./index-q5Up1lxZ-DJnIUFuC.js";import{c as p}from"./helpers-dt_t6yMe-BsSnQwjd.js";import"./vue.esm-bundler-UQm0beuB.js";import"./iframe-LGuXMrx9.js";/*!
+ * (C) Ionic http://ionicframework.com - MIT License
+ */const y=()=>{const o=window;o.addEventListener("statusTap",()=>{i(()=>{const n=o.innerWidth,r=o.innerHeight,e=document.elementFromPoint(n/2,r/2);if(!e)return;const t=m(e);t&&new Promise(s=>p(t,s)).then(()=>{a(async()=>{t.style.setProperty("--overflow","hidden"),await c(t,300),t.style.removeProperty("--overflow")})})})})};export{y as startStatusTap};
