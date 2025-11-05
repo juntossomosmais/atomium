ca2e89cbca247721baba24890c7b680661b0e7ba
felipefialho <felipefialho@users.noreply.github.com>
2025-11-05 20:54:34 +0000

    Deploy preview for PR 793 🛫

--- pr-preview/pr-793/react/assets/pagination.react.stories-Bcbgl5St.js
+++
@@ -1,6 +0,0 @@
-import{D as n}from"./index-Dcgwn9tW.js";import{C as a}from"./table-D060eKuu.js";import"./iframe-Ao1sNE2_.js";import"./preload-helper-PPVm8Dsz.js";const e={parameters:{docs:{description:{component:"A pagination component is an interface that allows users to navigate through large amounts of content that has been split into multiple pages"}}},argTypes:{page:{control:"number",description:"Current page.",table:{category:a.PROPERTIES}},count:{control:"number",description:"Total pages.",table:{category:a.PROPERTIES}}}},r={page:1,count:10},m={title:"Components/Pagination",component:n,...e},s=t=>h(n,{count:t.count,page:t.page}),o={render:t=>s(t),args:{...r}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
-  render: args => createPagination(args),
-  args: {
-    ...PaginationComponentArgs
-  }
-}`,...o.parameters?.docs?.source}}};const u=["Default"];export{o as Default,u as __namedExportsOrder,m as default};
