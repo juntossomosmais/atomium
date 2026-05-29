b66c77691d3c32a837e86dd3dea1b6a432bf1fc3
felipefialho <felipefialho@users.noreply.github.com>
2026-05-29 18:19:43 +0000

    Deploy preview for PR 633 🛫

---
+++ pr-preview/pr-633/assets/chip.core.stories-2caT7f7J.js
@@ -0,0 +1,42 @@
+import{n as e}from"./rolldown-runtime-BVpWp9h4.js";import{at as t,st as n}from"./storybook-BXAm_t_n.js";import{n as r,t as i}from"./table-DMN4TomJ.js";import{n as a,t as o}from"./decorator-Yp4X1xaW.js";var s,c,l=e((()=>{r(),o(),s={decorators:[a],parameters:{actions:{handles:[`atomClick`]},docs:{description:{component:`Wrapper of Ionic Chip component. Read the [Ionic documentation](https://ionicframework.com/docs/api/chip) for more information about the available properties and possibilities.`}}},argTypes:{disabled:{control:`boolean`,defaultValue:{summary:!1},description:`If true, the user cannot interact with the chip.`,table:{category:i.PROPERTIES}},activated:{control:`boolean`,defaultValue:{summary:!1},description:`If true, the chip will be activated.`,table:{category:i.PROPERTIES}},icon:{control:`text`,description:`If have an value, the chip will have an icon.`,table:{category:i.PROPERTIES}},close:{control:`boolean`,defaultValue:{summary:!1},description:`If true, the chip will have a close button.`,table:{category:i.PROPERTIES}},mode:{control:`select`,options:[`md`,`ios`],defaultValue:{summary:`md`},description:`The mode determines which platform styles to use.`,table:{category:i.PROPERTIES}},atomClick:{action:`atomClick`,description:`Emitted when the chip is clicked.`,table:{category:`Events`}}}},c={mode:`md`,disabled:!1,activated:!1,close:!1,label:`Label`}})),u,d,f,p,m,h,g,_;e((()=>{t(),l(),u={title:`Components/Chip`,...s},d=e=>n`
+    <atom-chip
+      color=${e.color}
+      disabled=${e.disabled}
+      activated=${e.activated}
+      focus=${e.focus}
+      mode=${e.mode}
+      icon=${e.icon}
+      close=${e.close}
+    >
+      ${e.label}
+    </atom-chip>
+  `,f={render:e=>d(e),args:{...c}},p={render:e=>d(e),args:{...c,disabled:!0}},m={render:e=>d(e),args:{...c,activated:!0}},h={render:e=>d(e),args:{...c,icon:`check`}},g={render:e=>d(e),args:{...c,close:!0}},f.parameters={...f.parameters,docs:{...f.parameters?.docs,source:{originalSource:`{
+  render: args => createChip(args),
+  args: {
+    ...ChipComponentArgs
+  }
+}`,...f.parameters?.docs?.source}}},p.parameters={...p.parameters,docs:{...p.parameters?.docs,source:{originalSource:`{
+  render: args => createChip(args),
+  args: {
+    ...ChipComponentArgs,
+    disabled: true
+  }
+}`,...p.parameters?.docs?.source}}},m.parameters={...m.parameters,docs:{...m.parameters?.docs,source:{originalSource:`{
+  render: args => createChip(args),
+  args: {
+    ...ChipComponentArgs,
+    activated: true
+  }
+}`,...m.parameters?.docs?.source}}},h.parameters={...h.parameters,docs:{...h.parameters?.docs,source:{originalSource:`{
+  render: args => createChip(args),
+  args: {
+    ...ChipComponentArgs,
+    icon: 'check'
+  }
+}`,...h.parameters?.docs?.source}}},g.parameters={...g.parameters,docs:{...g.parameters?.docs,source:{originalSource:`{
+  render: args => createChip(args),
+  args: {
+    ...ChipComponentArgs,
+    close: true
+  }
+}`,...g.parameters?.docs?.source}}},_=[`Default`,`Disabled`,`Activated`,`WithIcon`,`WithClose`]}))();export{m as Activated,f as Default,p as Disabled,g as WithClose,h as WithIcon,_ as __namedExportsOrder,u as default};
---
+++ pr-preview/pr-633/assets/list-slider.core.stories-C2168X_5.js
@@ -0,0 +1,56 @@
+import{n as e}from"./rolldown-runtime-BVpWp9h4.js";import{at as t,st as n}from"./storybook-BXAm_t_n.js";import{n as r,t as i}from"./table-DMN4TomJ.js";import{n as a,t as o}from"./decorator-Yp4X1xaW.js";var s=e((()=>{})),c,l=e((()=>{r(),o(),c={parameters:{docs:{description:{component:`List Slider is a component that displays a list of items in a horizontal slider.`}},actions:{handles:[`clickNext`,`clickPrev`]}},decorators:[a],argTypes:{hasNavigation:{description:"If `true`, the carousel will have navigation buttons.",defaultValue:{summary:!0},table:{category:i.PROPERTIES}},centralized:{description:"If `true`, the items will be centralized in the slider when do not fit in the viewport.",defaultValue:{summary:!1},table:{category:i.PROPERTIES}},step:{description:`Number of items to advance or go back when navigating.`,defaultValue:{summary:1},control:{type:`number`},table:{category:i.PROPERTIES}},"--slider-height":{description:`The min-height of the slider.`,defaultValue:{summary:`42px`},table:{category:i.CSS_CUSTOM_PROPERTIES}},"--slider-gap":{description:`Gap between each item in the slider.`,defaultValue:{summary:`--spacing-base`},table:{category:i.CSS_CUSTOM_PROPERTIES}},"--button-navigation-size":{description:`The size of the navigation buttons.`,defaultValue:{summary:`--spacing-xxxxlarge`},table:{category:i.CSS_CUSTOM_PROPERTIES}},clickNext:{action:`clickNext`,description:`Emitted when the next button is clicked. The event detail is the click event.`,table:{category:i.EVENTS}},clickPrev:{action:`clickPrev`,description:`Emitted when the previous button is clicked. The event detail is the click event.`,table:{category:i.EVENTS}},'CustomEvent("transitionend")':{description:`The event dispatched when transition ends to use in the parent component, for example for add a class to the current slide or add a lazy load to the images.`,table:{category:i.EVENTS}}}}})),u,d,f,p,m;e((()=>{t(),s(),l(),u={title:`Components/List Slider`,...c},d=e=>n`
+    <atom-list-slider step="${e.step}">
+      <atom-list-slider-item class="slide">
+        <div>Example 1</div>
+      </atom-list-slider-item>
+      <atom-list-slider-item class="slide">
+        <div>Example 2</div>
+      </atom-list-slider-item>
+      <atom-list-slider-item class="slide">
+        <div>Example 3</div>
+      </atom-list-slider-item>
+      <atom-list-slider-item class="slide">
+        <div>Example 4</div>
+      </atom-list-slider-item>
+      <atom-list-slider-item class="slide">
+        <div>Example 5</div>
+      </atom-list-slider-item>
+      <atom-list-slider-item class="slide">
+        <div>Example 6</div>
+      </atom-list-slider-item>
+      <atom-list-slider-item class="slide">
+        <div>Example 7</div>
+      </atom-list-slider-item>
+      <atom-list-slider-item class="slide">
+        <div>Example 8</div>
+      </atom-list-slider-item>
+      <atom-list-slider-item class="slide">
+        <div>Example 9</div>
+      </atom-list-slider-item>
+      <atom-list-slider-item class="slide">
+        <div>Example 10</div>
+      </atom-list-slider-item>
+    </atom-list-slider>
+  `,f={render:e=>d(e)},p={render:()=>n`
+    <atom-list-slider centralized="true">
+      <atom-list-slider-item class="slide">
+        <div>Example 1</div>
+      </atom-list-slider-item>
+      <atom-list-slider-item class="slide">
+        <div>Example 2</div>
+      </atom-list-slider-item>
+    </atom-list-slider>
+  `},f.parameters={...f.parameters,docs:{...f.parameters?.docs,source:{originalSource:`{
+  render: args => createListSlider(args)
+}`,...f.parameters?.docs?.source}}},p.parameters={...p.parameters,docs:{...p.parameters?.docs,source:{originalSource:`{
+  render: () => html\`
+    <atom-list-slider centralized="true">
+      <atom-list-slider-item class="slide">
+        <div>Example 1</div>
+      </atom-list-slider-item>
+      <atom-list-slider-item class="slide">
+        <div>Example 2</div>
+      </atom-list-slider-item>
+    </atom-list-slider>
+  \`
+}`,...p.parameters?.docs?.source}}},m=[`Default`,`Centralized`]}))();export{p as Centralized,f as Default,m as __namedExportsOrder,u as default};
