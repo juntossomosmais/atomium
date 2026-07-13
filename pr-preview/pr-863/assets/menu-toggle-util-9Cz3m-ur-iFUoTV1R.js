c03b5c7ffe71c5053a47fc20a9e9ac90e5461bc0
igorwessel <igorwessel@users.noreply.github.com>
2026-07-13 14:03:45 +0000

    Deploy preview for PR 863 🛫

---
+++ pr-preview/pr-863/assets/menu-toggle-util-9Cz3m-ur-iFUoTV1R.js
@@ -0,0 +1,1 @@
+import{n as e}from"./rolldown-runtime-DaJ6WEGw.js";import{n as t,t as n}from"./index-BbCl3y2Z-f30VfTwK.js";var r,i=e((()=>{n(),r=async e=>{let n=await t.get(e);return!!(n&&await n.isActive())}}));export{r as n,i as t};
